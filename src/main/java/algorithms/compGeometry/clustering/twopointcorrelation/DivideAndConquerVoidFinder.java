b54212b04bcf88b868b48f3974e48d5faff714ba
nichole <nichole.king@gmail.com>
2013-12-15 12:54:00 -0800

    editing javadoc comments

--- src/main/java/algorithms/compGeometry/clustering/twopointcorrelation/DivideAndConquerVoidFinder.java
+++ src/main/java/algorithms/compGeometry/clustering/twopointcorrelation/DivideAndConquerVoidFinder.java
@@ -18,8 +18,15 @@ public class DivideAndConquerVoidFinder extends AbstractVoidFinder {
 
     /**
      * A divide and conquer approach to finding the rectangular areas
-     * containing only two points.  it's a recursion with pattern
-     * T(n) = 4T(n/2) + n  so the runtime is O(n^2).  
+     * containing only two points.  
+     *
+     * runtime complexity:
+     *     processIndexedRegion is a little worse than linear
+     *     T(processIndexedRegion) = O(N)
+     *
+     *     then O(findVoids) = (c000 + c001) + (c005)*( O(N) ) + c003*T(findVoids) + c004*T(findVoids)
+     *                       = 2T(N/2) + N
+     *                       ==> this has a solution that is O(N lg2 N)
      * 
      * Note: It does not completely sample every pair of points.
      *
