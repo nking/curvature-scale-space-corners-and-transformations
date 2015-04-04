197f8830eb6a4b1861ae694e88db97db0c59b1d5
nichole <nichole@climbwithyourfeet.com>
2015-04-03 17:19:04 -0700

    added notes for changing the embedded gaps check in the new PerimeterFinder method.  currently, it is checking row by row, but needs to consider the contiguous gaps as a whole, that is, the embedded check needs to invalidate or validate the entire contiguous embedded gap.

--- src/algorithms/compGeometry/PerimeterFinder.java
+++ src/algorithms/compGeometry/PerimeterFinder.java
@@ -216,6 +216,127 @@ public class PerimeterFinder {
         return rowColRange;
     }
    
+    /**
+     * For the given points, find the ranges of columns that bound the points
+     * that are contiguous and the points that are completely 
+     * enclosed within points but not part of the set.
+     * This returns an outline of the points attempting to correct for
+     * concave portions of the hull, that is, it is roughly a concave hull
+     * that includes embedded PairInts that are not in the set points.
+     * 
+     * @param points
+     * @param outputRowMinMax output populated as the min and max of rows are 
+     * determined.
+     * @param outputEmbeddedGapPoints a return variable holding the found 
+     * embedded points that are not in the "points" set, but are enclosed by it.
+     * @return 
+     */
+    public Map<Integer, List<PairInt>> find2(Set<PairInt> points, 
+        int[] outputRowMinMax, Set<PairInt> outputEmbeddedGapPoints) {
+        
+        if (points == null) {
+	    	throw new IllegalArgumentException("points cannot be null");
+        }
+        if (outputRowMinMax == null) {
+	    	throw new IllegalArgumentException("outputRowMinMax cannot be null");
+        }
+        
+        //O(N):
+        int[] minMaxXY = MiscMath.findMinMaxXY(points);
+        int minX = minMaxXY[0];
+        int maxX = minMaxXY[1];
+        int minY = minMaxXY[2];
+        int maxY = minMaxXY[3];
+        
+        outputRowMinMax[0] = minY;
+        outputRowMinMax[1] = maxY;       
+        
+        // key holds row number
+        // value holds (first column number, last column number) for points in the row
+        Map<Integer, List<PairInt>> rowColRanges = findRowColRanges(points, 
+            minX, maxX, minY, maxY);        
+        
+        /* TODO: alter algorithm to look at whether the contiguous gap pixels
+        are unbounded, rather than looking at each gap along a row.
+        ... in progress:
+        -find contiguous gaps within rowColRanges
+        -store those as groups of Map<Integer, List<PairInt>>
+        -for each contiguous gap group, visit similarly as below to
+            assert that each gap in a row is bounded above and below
+            if there is any that is not bounded, skip the rest of the group,
+            else include the entire group in the mergeIndexes with row information
+            to condense the values in rowColRanges and possibly
+            return the embedded gap points too
+        */
+        
+        
+        // now, want to find gaps in the colRanges where there are points
+        // in "points" above it and below it (i.e. the gap is completely embedded
+        // in "points")
+        //
+        // but we also need to remove the contiguous gap when any of it's 
+        // containing row colRanges is unbounded...
+        
+        for (int row = minY; row <= maxY; row++) {
+            
+            Integer key = Integer.valueOf(row);
+            
+            List<PairInt> colRanges = rowColRanges.get(key);
+            
+            List<Integer> mergeIndexes = new ArrayList<Integer>();
+            
+            for (int i = 1; i < colRanges.size(); i++) {
+                
+                int gapStart = colRanges.get(i - 1).getY() + 1;
+                int gapStop = colRanges.get(i).getX() - 1;
+                
+                boolean bounded = boundedByPointsInHigherRows(row, gapStart,
+                    gapStop, maxY, rowColRanges);
+                
+                if (!bounded) {
+                    continue;
+                }
+                
+                bounded = boundedByPointsInLowerRows(row, gapStart,
+                    gapStop, minY, rowColRanges);
+                
+                if (!bounded) {
+                    continue;
+                }
+                
+                // the gap is bounded above and below, so
+                // combine colRange before with current colRange
+                mergeIndexes.add(Integer.valueOf(i));
+            }
+            
+            if (!mergeIndexes.isEmpty()) {
+                
+                for (int i = (mergeIndexes.size() - 1); i > -1; i--) {
+                    
+                    int idx = mergeIndexes.get(i).intValue();
+                    
+                    PairInt edit = colRanges.get(idx - 1);
+                    
+                    int gapStart = edit.getY() + 1;
+                    
+                    PairInt current = colRanges.get(idx);
+                    
+                    int gapStop = current.getX() - 1;
+                    
+                    edit.setY(current.getY());
+                                        
+                    colRanges.remove(idx);
+                    
+                    for (int cIdx = gapStart; cIdx <= gapStop; cIdx++) {
+                        outputEmbeddedGapPoints.add(new PairInt(cIdx, row));
+                    }
+                }
+            }
+        }
+        
+        return rowColRanges;
+    }
+    
     boolean boundedByPointsInHigherRows(int row, int gapStart, int gapStop,
         int maxRow, Map<Integer, List<PairInt>> rowColRange) {
         
@@ -245,7 +366,7 @@ public class PerimeterFinder {
         for (int r = (row - 1); r >= minRow; r--) {
             
             List<PairInt> colRanges = rowColRanges.get(Integer.valueOf(r));
-            
+
             boolean bounded = boundedByPoints(colRanges, gapRange, remove);
             
             if (bounded) {
