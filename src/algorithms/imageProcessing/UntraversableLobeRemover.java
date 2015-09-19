cd04eab35d8b5eb22f8d4ea750ba6af46d0957b9
nking <nichole@climbwithyourfeet.com>
2015-09-18 20:54:49 -0700

    work towards a closed curve improvement method.

--- src/algorithms/imageProcessing/UntraversableLobeRemover.java
+++ src/algorithms/imageProcessing/UntraversableLobeRemover.java
@@ -11,7 +11,21 @@ class UntraversableLobeRemover {
 
     public boolean applyFilter(Set<PairInt> borderPixels) {
         
+        /*
+        Example:
+                         -3
+                 #       -2
+              -  #  -    -1    The center pattern can be found, then if each
+           #  #  -  -     0    of the 3 has a neighbor that isn't adjacent
+              -  #  -     1    to the others, it is a junction that cannot
+                    #          be traversed.
         
+        0 -1  0  1  2
+
+
+        To further locate the untraversable section,
+
+        */
         
         return false;
     }
