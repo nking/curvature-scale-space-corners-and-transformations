a0ec72b01477acac34a6e9bf93edae4dc573f0d5
nking <nichole@climbwithyourfeet.com>
2015-09-26 22:11:44 -0700

    added a debug output image for the distance transform

--- src/main/java/com/climbwithyourfeet/clustering/DTClusterFinder.java
+++ src/main/java/com/climbwithyourfeet/clustering/DTClusterFinder.java
@@ -1,7 +1,14 @@
 package com.climbwithyourfeet.clustering;
 
 import com.climbwithyourfeet.clustering.util.PairInt;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  * main class to cluster finder whose logic is based upon distance transform,
@@ -20,7 +27,7 @@ public class DTClusterFinder {
     private float critDens = Float.POSITIVE_INFINITY;
     
     private DTGroupFinder groupFinder = null;
-    
+
     private enum STATE {
         INIT, HAVE_CLUSTER_DENSITY, HAVE_GROUPS
     }
@@ -54,7 +61,14 @@ public class DTClusterFinder {
         CriticalDensitySolver densSolver = new CriticalDensitySolver();
         
         if (debug) {
+            
             densSolver.setToDebug();
+            
+            try {
+                writeDebugImage(dt, Long.toString(System.currentTimeMillis()));
+            } catch (IOException ex) {
+                Logger.getLogger(DTClusterFinder.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
         this.critDens = densSolver.findCriticalDensity(dt, points.size(), width, height);               
@@ -108,6 +122,47 @@ public class DTClusterFinder {
     
     public float getCriticalDensity() {
         return critDens;
+    }
+    
+    private void writeDebugImage(int[][] dt, String fileSuffix) throws IOException {
+        
+        BufferedImage outputImage = new BufferedImage(width, height, 
+            BufferedImage.TYPE_INT_RGB);
+        
+        for (int i = 0; i < dt.length; ++i) {
+            for (int j = 0; j < dt[0].length; ++j) {
+                int v = dt[i][j];
+                int rgb = (int)(((v & 0x0ff) << 16)
+                    | ((v & 0x0ff) << 8) | (v & 0x0ff));
+                outputImage.setRGB(i, j, rgb);
+            }
+        }
+        
+        // write to an output directory.  we have user.dir from system properties
+        // but no other knowledge of users's directory structure
+        URL baseDirURL = this.getClass().getClassLoader().getResource(".");
+        String baseDir = null;
+        if (baseDirURL != null) {
+            baseDir = baseDirURL.getPath();
+        } else {
+            baseDir = System.getProperty("user.dir");
+        }
+        if (baseDir == null) {
+            return;
+        }
+        File t = new File(baseDir + "/bin");
+        if (t.exists()) {
+            baseDir = t.getPath();
+        } else if ((new File(baseDir + "/test")).exists()) {
+            baseDir = baseDir + "/test";
+        }
+        
+        // no longer need to use file.separator
+        String outFilePath = baseDir + "/distance_transform_" + fileSuffix + ".png";
+        
+        ImageIO.write(outputImage, "PNG", new File(outFilePath));
+        
+        Logger.getLogger(this.getClass().getName()).info("wrote " + outFilePath);
     }
     
 }
