aede524cf858463a00dc727925fc9beb1f7d05ba
nking <nichole@climbwithyourfeet.com>
2015-10-02 12:38:47 -0700

    new class to hold image segmentation methods present elsewhere

---
+++ src/algorithms/imageProcessing/ImageSegmentation.java
@@ -0,0 +1,12 @@
+package algorithms.imageProcessing;
+
+/**
+ * class holding several different image segmentation methods.  Note that
+ * some other techniques involving contrast for example, are elsewhere.
+ * 
+ * @author nichole
+ */
+public class ImageSegmentation {
+    
+    
+}
