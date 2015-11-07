3c8c393a9c56d6729db69e3a1b991c5b2b69d7b0
nking <nichole@climbwithyourfeet.com>
2015-11-06 22:28:05 -0800

    added one more set of tests to the encapsulation of the scale calculator thru feature matcher.

--- tests/algorithms/imageProcessing/FeatureMatcherWrapperTest.java
+++ tests/algorithms/imageProcessing/FeatureMatcherWrapperTest.java
@@ -4,8 +4,8 @@ import algorithms.misc.MiscDebug;
 import algorithms.util.PairInt;
 import algorithms.util.ResourceFinder;
 import java.util.Collection;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.*;
 
 /**
@@ -14,11 +14,13 @@ import static org.junit.Assert.*;
  */
 public class FeatureMatcherWrapperTest extends TestCase {
     
+    private Logger log = Logger.getLogger(this.getClass().getName());
+    
     public FeatureMatcherWrapperTest() {
     }
     
     public void test0() throws Exception {
-        
+                
         String fileName1, fileName2;
         
         for (int i = 0; i < 4;/*3;*/ ++i) {
@@ -44,12 +46,43 @@ public class FeatureMatcherWrapperTest extends TestCase {
                     break;
                 }
             }
-            runCorrespondenceList(fileName1, fileName2);
+            runCorrespondenceList(fileName1, fileName2, false);
+        }
+    }
+    
+    public void testRot90() throws Exception {
+                
+        String fileName1, fileName2;
+        
+        for (int i = 0; i < 4;/*3;*/ ++i) {
+            switch(i) {
+                case 0: {
+                    fileName1 = "brown_lowe_2003_image1.jpg";
+                    fileName2 = "brown_lowe_2003_image2.jpg";
+                    break;
+                }
+                case 1: {
+                    fileName1 = "venturi_mountain_j6_0001.png";
+                    fileName2 = "venturi_mountain_j6_0010.png";
+                    break;
+                }
+                case 2: {
+                    fileName1 = "books_illum3_v0_695x555.png";
+                    fileName2 = "books_illum3_v6_695x555.png";
+                    break;
+                }
+                default: {
+                    fileName1 = "campus_010.jpg";
+                    fileName2 = "campus_011.jpg";
+                    break;
+                }
+            }
+            runCorrespondenceList(fileName1, fileName2, true);
         }
     }
 
-    private void runCorrespondenceList(String fileName1, String fileName2) 
-        throws Exception {
+    private void runCorrespondenceList(String fileName1, String fileName2, 
+        boolean rotateBy90) throws Exception {
         
         int idx = fileName1.lastIndexOf(".");
         String fileName1Root = fileName1.substring(0, idx);
@@ -60,6 +93,19 @@ public class FeatureMatcherWrapperTest extends TestCase {
         ImageExt img1 = ImageIOHelper.readImageExt(filePath1);
         String filePath2 = ResourceFinder.findFileInTestResources(fileName2);
         ImageExt img2 = ImageIOHelper.readImageExt(filePath2);
+        
+        if (rotateBy90) {
+            TransformationParameters params90 = new TransformationParameters();
+            params90.setRotationInDegrees(90);
+            params90.setOriginX(0);
+            params90.setOriginY(0);
+            params90.setTranslationX(0);
+            params90.setTranslationY(img1.getWidth() - 1);
+            Transformer transformer = new Transformer();
+            img1 = (ImageExt) transformer.applyTransformation(img1,
+                params90, img1.getHeight(), img1.getWidth());
+            int z = 1;
+        }
         
         FeatureMatcherWrapper wrapper = new FeatureMatcherWrapper(img1, img2,
             fileName1Root);
@@ -72,8 +118,29 @@ public class FeatureMatcherWrapperTest extends TestCase {
         Collection<PairInt> m2 = cl.getPoints2();
         GreyscaleImage gsImg1 = img1.copyToGreyscale();
         GreyscaleImage gsImg2 = img2.copyToGreyscale();
-        MiscDebug.plotCorners(gsImg1, m1, "1_" + fileName1Root  + "_matched", 2);
-        MiscDebug.plotCorners(gsImg2, m2, "2_" + fileName2Root + "_matched", 2);
-                
+        String name1 = "1_" + fileName1Root;
+        String name2 = "2_" + fileName2Root;
+        if (rotateBy90) {
+            name1 = name1 + "_r90";
+            name2 = name2 + "_r90";
+        }
+        name1 = name1 + "_matched";
+        name2 = name2 + "_matched";
+        MiscDebug.plotCorners(gsImg1, m1, name1, 2);
+        MiscDebug.plotCorners(gsImg2, m2, name2, 2);
+        
+        float scale = cl.getScale();
+        int rotationInDegrees = cl.getRotationInDegrees();
+        
+        log.info("scale for " + fileName1 + " =" + scale + " rotationDeg=" + 
+            rotationInDegrees);
+
+        if (fileName1.contains("brown_lowe_2003_image1")) {
+            // one portion of image scale is ~ 0.9
+            assertTrue(Math.abs(scale - 1) < 0.15);
+        } else {
+            assertTrue(Math.abs(scale - 1) < 0.12);
+        }
+
     }
 }
