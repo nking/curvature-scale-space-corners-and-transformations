a9f28fbb5f99070ab3963f32fd278a5af6f1ff12
nking <nichole@climbwithyourfeet.com>
2016-01-06 21:10:36 -0800

    added a couple of methods to explore the hough transforms of corners alone.

--- src/algorithms/compGeometry/HoughTransform.java
+++ src/algorithms/compGeometry/HoughTransform.java
@@ -1,7 +1,9 @@
 package algorithms.compGeometry;
 
 import algorithms.MultiArrayMergeSort;
+import algorithms.imageProcessing.CornerRegion;
 import algorithms.imageProcessing.DFSSimilarThetaRadiusGroupsFinder;
+import algorithms.imageProcessing.IntensityFeatures;
 import algorithms.imageProcessing.MiscellaneousCurveHelper;
 import algorithms.misc.Misc;
 import algorithms.util.PairInt;
@@ -14,6 +16,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * a class for Hough transforms of simple geometric shapes.  currently a line
@@ -141,7 +145,188 @@ public class HoughTransform {
         
         return outputPolarCoordsPixMap;
     }
+    
+    /**
+     * given lists of corner regions, computes the Hough
+     * transform of lines and returns results lists of polar theta in degrees
+     * and radius as distance from
+     * the origin in pixels.
+     * Note that the angle is calculated for expectations of a
+     * counter clockwise ordered curve and the vector of the angle is 
+     * perpendicular to p1 (direction given by right hand rule).
+     * The angles are 0 to 360.
+     * 
+     * Note that if the edge has less than 3 points, an empty map is returned.
+     * 
+     * @param cornerLists
+     * @param features
+     * @return thetaRadiusPixCoords lists
+     */
+    public List<List<PairInt>> calculateRoughHoughTransforms(
+        List<List<CornerRegion>> cornerLists, IntensityFeatures features) {
         
+        // theta is 0 to 360
+        Map<Integer, Double> cosineMap = Misc.getCosineThetaMapForTwoPI();
+        Map<Integer, Double> sineMap = Misc.getSineThetaMapForTwoPI();
+                        
+        List<List<PairInt>> trLists = new ArrayList<List<PairInt>>();
+        
+        for (int cIdx = 0; cIdx < cornerLists.size(); ++cIdx) {
+        
+            List<CornerRegion> cornerList = cornerLists.get(cIdx);
+            
+            List<PairInt> trList = new ArrayList<PairInt>();
+            
+            for (int i = 0; i < cornerList.size(); ++i) {
+            
+                CornerRegion cr = cornerList.get(i);
+                
+                int x = cr.getX()[cr.getKMaxIdx()];
+                int y = cr.getY()[cr.getKMaxIdx()];
+                
+                try {
+                    int tInt = features.calculate45DegreeOrientation(x, y);
+                    
+                    if (tInt > 359) {
+                        tInt = tInt - 360;
+                    }
+
+                    Integer theta = Integer.valueOf(tInt);
+
+                    double ct = cosineMap.get(theta).doubleValue();
+                    double st = sineMap.get(theta).doubleValue();
+
+                    double r = (x * ct) + (y * st);
+
+                    if (r < 0) {
+                        r *= -1;
+                    }
+
+                    PairInt p = new PairInt(tInt, (int)Math.round(r));
+                    trList.add(p);
+                } catch (CornerRegion.CornerRegionDegneracyException ex) {
+                    Logger.getLogger(HoughTransform.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            trLists.add(trList);
+        }
+        
+        return trLists;
+    }
+    /**
+     * given lists of corner regions, computes the Hough
+     * transform of lines and returns results lists of polar theta in degrees
+     * and radius as distance from
+     * the origin in pixels.
+     * Note that the angle is calculated for expectations of a
+     * counter clockwise ordered curve and the vector of the angle is 
+     * perpendicular to p1 (direction given by right hand rule).
+     * The angles are 0 to 360.
+     * 
+     * Note that if the edge has less than 3 points, an empty map is returned.
+     * 
+     * @param cornerLists
+     * @param edges
+     * @return thetaRadiusPixCoords lists
+     */
+    public List<List<PairInt>> calculateRoughHoughTransforms(
+        List<List<CornerRegion>> cornerLists, List<PairIntArray> edges) {
+        
+        // theta is 0 to 360
+        Map<Integer, Double> cosineMap = Misc.getCosineThetaMapForTwoPI();
+        Map<Integer, Double> sineMap = Misc.getSineThetaMapForTwoPI();
+                        
+        List<List<PairInt>> trLists = new ArrayList<List<PairInt>>();
+        
+        MiscellaneousCurveHelper curveHelper = new MiscellaneousCurveHelper();
+                
+        for (int cIdx = 0; cIdx < cornerLists.size(); ++cIdx) {
+        
+            PairIntArray edge = edges.get(cIdx);
+            int nEdge = edge.getN();
+            
+            boolean curveIsClosed = (edge instanceof PairIntArrayWithColor) &&
+                (((PairIntArrayWithColor)edge).getColor() == 1);
+
+            List<CornerRegion> cornerList = cornerLists.get(cIdx);
+            
+            List<PairInt> trList = new ArrayList<PairInt>();
+            
+            for (int i = 0; i < cornerList.size(); ++i) {
+            
+                CornerRegion cr = cornerList.get(i);
+                
+                int eIdx = cr.getIndexWithinCurve();
+                if (eIdx == -1) {
+                    continue;
+                }
+                int x = cr.getX()[cr.getKMaxIdx()];
+                int y = cr.getY()[cr.getKMaxIdx()];
+                
+                int xp, yp, xn, yn;
+
+                if (eIdx == 0) {
+                    if (curveIsClosed) {
+                        xp = edge.getX(nEdge - 1);
+                        yp = edge.getY(nEdge - 1);
+                    } else {
+                        // use replication for boundary
+                        xp = x;
+                        yp = y;
+                    }
+                    xn = edge.getX(eIdx + 1);
+                    yn = edge.getY(eIdx + 1);
+                } else if (eIdx == (nEdge - 1)) {
+                    xp = edge.getX(eIdx - 1);
+                    yp = edge.getY(eIdx - 1);
+                    if (curveIsClosed) {
+                        xn = edge.getX(0);
+                        yn = edge.getY(0);
+                    } else {
+                        xn = x;
+                        yn = y;
+                    }
+                } else {
+                    xp = edge.getX(eIdx - 1);
+                    yp = edge.getY(eIdx - 1);
+
+                    xn = edge.getX(eIdx + 1);
+                    yn = edge.getY(eIdx + 1);
+                }
+
+                // note, this is not the angle along the edge, it's perpendicular
+                // to it, but the calculation is consistent
+                double t = curveHelper.calculateAngleTangentToMidpoint(xp, yp, x, y,
+                    xn, yn);
+
+                double tDegrees = t * 180. / Math.PI;
+
+                int tInt = (int) Math.round(tDegrees);
+
+                if (tInt > 359) {
+                    tInt = tInt - 360;
+                }
+
+                Integer theta = Integer.valueOf(tInt);
+
+                double ct = cosineMap.get(theta).doubleValue();
+                double st = sineMap.get(theta).doubleValue();
+
+                double r = (x * ct) + (y * st);
+
+                if (r < 0) {
+                    r *= -1;
+                }
+
+                PairInt p = new PairInt(tInt, (int) Math.round(r));
+
+                trList.add(p);
+            }
+            trLists.add(trList);
+        }
+        
+        return trLists;
+    }
     public List<PairInt> sortByVotes(Map<PairInt, Set<PairInt>> thetaRadiusPixMap) {
         
         int[] votes = new int[thetaRadiusPixMap.size()];
