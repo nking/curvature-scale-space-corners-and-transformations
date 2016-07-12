b0a9b5261e4b2431421e11e7cb845960d89eb8b6
nichole <nichole@climbwithyourfeet.com>
2016-07-12 16:27:40 -0700

    starting tests for methods in MedialAxis1

---
+++ tests/algorithms/compGeometry/MedialAxis1Test.java
@@ -0,0 +1,28 @@
+package algorithms.compGeometry;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author nichole
+ */
+public class MedialAxis1Test extends TestCase {
+    
+    public MedialAxis1Test() {
+    }
+    
+    /*
+    Figure 4 of 
+    "Efficient Computation of A Simplified Medial Axis"
+        by Foskey, Lin, and Manocha
+               ___
+               | |
+      _________| |_______
+      |                 |
+      |                 |
+      |                 |
+      |                 |
+      |_________________|
+    
+    */
+}
