4eef1b1766d4f2b128ab6791a9a504c8b075ff1f
nichole <nichole@climbwithyourfeet.com>
2016-10-05 11:39:27 -0700

    more IntegralHistograms unit tests

--- tests/algorithms/imageProcessing/IntegralHistogramsTest.java
+++ tests/algorithms/imageProcessing/IntegralHistogramsTest.java
@@ -285,169 +285,162 @@ public class IntegralHistogramsTest extends TestCase {
         assertTrue(Arrays.equals(expectedWindow, output));
         assertEquals(1, outputN[0]);
         
-        
         /*
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 1, 5, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
-        
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 1, 3, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
-        
-        expectedSum = 1;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 1, 1, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 1);
-        */
-        //0,0  0,1  0,2  1,0  1,1  1,2
-        
-        /*
-        img              sImg
-        values           values
-        2  5  2  7     10 18  36
-        1  3  1  2      5 11  22
-        0  2  5  9      2  7  16
-           0  1  2
-        */
-        /*
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 2, 5, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
-        
-        expectedSum = 20;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 2, 3, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 6);
-        
-        expectedSum = 2;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            1, 2, 1, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 1);
+        column major notation is used
+        2  155  2   7      6  7  8
+        1  3   16  42      3  4  5
+        0  2    5   9      0  1  2
+           0    1   2
+        pixIdx = (row * width) + col
+        0,0 = 0  1,0 = 1  2,0 = 2
+        0,1 = 3  1,1 = 4  2,1 = 5
+        0,2 = 6  1,2 = 7  2,2 = 8
         */
         
-        /*
-        img              sImg
-        values           values
-        2  5  2  7     10 18  36
-        1  3  1  2      5 11  22
-        0  2  5  9      2  7  16
-           0  1  2
-        */
-        /*
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 0, 5, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
+        // window=5
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 1, 2, 5, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(9, outputN[0]);
         
-        expectedSum = 17;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 0, 3, output);
-        assertEquals(expectedSum, output[0]);
-        assertEquals(4, output[1]);
+        // window=3
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 1, 2, 3, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        sumTable.subtract(expectedWindow, sHists[2]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(6, outputN[0]);
         
-        expectedSum = 9;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 0, 1, output);
-        assertEquals(expectedSum, output[0]);
-        assertEquals(1, output[1]);
-        */
-        
-        /*
-        img              sImg
-        values           values
-        2  5  2  7     10 18  36
-        1  3  1  2      5 11  22
-        0  2  5  9      2  7  16
-           0  1  2
-        */
-        /*
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 1, 5, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
-        
-        expectedSum = 26;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 1, 3, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 6);
-        
-        expectedSum = 2;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 1, 1, output);
-        assertEquals(expectedSum, output[0]);
-        assertEquals(1, output[1]);
-        */
+        // window=1
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 1, 2, 1, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[7]);
+        sumTable.subtract(expectedWindow, sHists[6]);
+        sumTable.subtract(expectedWindow, sHists[4]);
+        sumTable.add(expectedWindow, sHists[3]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(1, outputN[0]);
         
         /*
-        img              sImg
-        values           values
-        2  5  2  7     10 18  36
-        1  3  1  2      5 11  22
-        0  2  5  9      2  7  16
-           0  1  2
+        column major notation is used
+        2  155  2   7      6  7  8
+        1  3   16  42      3  4  5
+        0  2    5   9      0  1  2
+           0    1   2
+        pixIdx = (row * width) + col
+        0,0 = 0  1,0 = 1  2,0 = 2
+        0,1 = 3  1,1 = 4  2,1 = 5
+        0,2 = 6  1,2 = 7  2,2 = 8
         */
+        // window=5
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 0, 5, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(9, outputN[0]);
+        
+        // window=3
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 0, 3, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[5]);
+        sumTable.subtract(expectedWindow, sHists[3]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(4, outputN[0]);
+        
+        // window=1
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 0, 1, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[2]);
+        sumTable.subtract(expectedWindow, sHists[1]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(1, outputN[0]);
+        
         /*
-        expectedSum = 36;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 2, 5, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 9);
-        
-        expectedSum = 12;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 2, 3, output);
-        assertEquals(expectedSum, output[0]);
-        assertEquals(4, output[1]);
-        
-        expectedSum = 7;
-        sumTable.extractWindowFromSummedAreaTable(sImg, 
-            2, 2, 1, output);
-        assertEquals(output[0], expectedSum);
-        assertEquals(output[1], 1);
-        
-        // --- assert the mean of window size 5 ----
-        GreyscaleImage mImg = sumTable.
-            applyMeanOfWindowFromSummedAreaTable(sImg, 5);
-        for (int i = 0; i < mImg.getNPixels(); ++i) {
-            assertEquals(36/9, mImg.getValue(i));
-        }
-        
-        // --- assert the mean of window size 3 ----
-        mImg = sumTable.applyMeanOfWindowFromSummedAreaTable(sImg, 3);
-        assertEquals(11/4, mImg.getValue(0, 0));
-        assertEquals(18/6, mImg.getValue(0, 1));
-        assertEquals(11/4, mImg.getValue(0, 2));
-        assertEquals(22/6, mImg.getValue(1, 0));
-        assertEquals(36/9, mImg.getValue(1, 1));
-        assertEquals(20/6, mImg.getValue(1, 2));
-        assertEquals(17/4, mImg.getValue(2, 0));
-        assertEquals(26/6, mImg.getValue(2, 1));
-        assertEquals(12/4, mImg.getValue(2, 2));
-        
-        // --- assert the mean of window size 1 ----
-        mImg = sumTable.applyMeanOfWindowFromSummedAreaTable(sImg, 1);
-        assertEquals(img.getValue(0, 0), mImg.getValue(0, 0));
-        assertEquals(img.getValue(0, 1), mImg.getValue(0, 1));
-        assertEquals(img.getValue(0, 2), mImg.getValue(0, 2));
-        assertEquals(img.getValue(1, 0), mImg.getValue(1, 0));
-        assertEquals(img.getValue(1, 1), mImg.getValue(1, 1));
-        assertEquals(img.getValue(1, 2), mImg.getValue(1, 2));
-        assertEquals(img.getValue(2, 0), mImg.getValue(2, 0));
-        assertEquals(img.getValue(2, 1), mImg.getValue(2, 1));
-        assertEquals(img.getValue(2, 2), mImg.getValue(2, 2));
+        column major notation is used
+        2  155  2   7      6  7  8
+        1  3   16  42      3  4  5
+        0  2    5   9      0  1  2
+           0    1   2
+        pixIdx = (row * width) + col
+        0,0 = 0  1,0 = 1  2,0 = 2
+        0,1 = 3  1,1 = 4  2,1 = 5
+        0,2 = 6  1,2 = 7  2,2 = 8
         */
+        // window=5
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 1, 5, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(9, outputN[0]);
+        
+        // window=3
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 1, 3, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        sumTable.subtract(expectedWindow, sHists[6]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(6, outputN[0]);
+        
+        // window=1
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 1, 1, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[5]);
+        sumTable.subtract(expectedWindow, sHists[4]);
+        sumTable.subtract(expectedWindow, sHists[2]);
+        sumTable.add(expectedWindow, sHists[1]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(1, outputN[0]);
+        
+        /*
+        column major notation is used
+        2  155  2   7      6  7  8
+        1  3   16  42      3  4  5
+        0  2    5   9      0  1  2
+           0    1   2
+        pixIdx = (row * width) + col
+        0,0 = 0  1,0 = 1  2,0 = 2
+        0,1 = 3  1,1 = 4  2,1 = 5
+        0,2 = 6  1,2 = 7  2,2 = 8
+        */
+        // window=5
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 2, 5, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(9, outputN[0]);
+        
+        // window=3
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 2, 3, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        sumTable.subtract(expectedWindow, sHists[6]);
+        sumTable.subtract(expectedWindow, sHists[2]);
+        sumTable.add(expectedWindow, sHists[0]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(4, outputN[0]);
+        
+        // window=1
+        sumTable.extractWindowFromIntegralHistograms(sHists, 
+            w, h, 2, 2, 1, output, outputN);
+        Arrays.fill(expectedWindow, 0);
+        sumTable.add(expectedWindow, sHists[8]);
+        sumTable.subtract(expectedWindow, sHists[7]);
+        sumTable.subtract(expectedWindow, sHists[5]);
+        sumTable.add(expectedWindow, sHists[4]);
+        assertTrue(Arrays.equals(expectedWindow, output));
+        assertEquals(1, outputN[0]);
+        
     }
 }
