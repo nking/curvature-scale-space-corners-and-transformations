4c9c04dcf900634992533c27e8c9677e8f962aa7
nichole <nichole@climbwithyourfeet.com>
2016-10-04 21:54:02 -0700

    added more to integral histograms

---
+++ tests/algorithms/imageProcessing/IntegralHistogramsTest.java
@@ -0,0 +1,14 @@
+package algorithms.imageProcessing;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author nichole
+ */
+public class IntegralHistogramsTest extends TestCase {
+    
+    public IntegralHistogramsTest() {
+    }
+    
+}
