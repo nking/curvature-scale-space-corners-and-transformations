531bf1c0651a1440bbe23046fc5ca0efb3d8ffc8
nichole <nichole@climbwithyourfeet.com>
2016-12-14 04:20:24 -0800

    adding another unit test for new class

--- tests/algorithms/imageProcessing/matching/OrderedClosedCurveCorrespondenceTest.java
+++ tests/algorithms/imageProcessing/matching/OrderedClosedCurveCorrespondenceTest.java
@@ -169,4 +169,68 @@ public class OrderedClosedCurveCorrespondenceTest extends TestCase {
         
     }
     
+    public void testAddIntervals4() {
+        
+        List<SR> intervals = new ArrayList<SR>();
+        
+         /*
+              0  1  2  3  4  5  6  7  8  9
+            9          -                    
+            8       -                    
+            7    -                    
+            6 -         
+            5                                  )
+            4                               +
+            3                            +
+            2                   0  
+            1                0   
+            0             0
+              0  1  2  3  4  5  6  7  8  9 10 11
+         
+        */
+        
+        /*
+        0 : 3    6 : 9  offset=6
+        4 : 6    0 : 2  offset=6
+        9 : 11   3 : 5  offset= 3 + (n2-1-9) + 1 = 4     
+        */
+        int n1 = 12; int n2 = 10;
+        List<PairInt> ranges = new ArrayList<PairInt>();
+        ranges.add(new PairInt(4, 6));
+        ranges.add(new PairInt(9, 11));
+        ranges.add(new PairInt(0, 3));
+        
+        for (int i = 0; i < ranges.size(); ++i) {
+            PairInt range = ranges.get(i);
+            SR sr = new SR();
+            sr.startIdx1 = range.getX(); sr.stopIdx1 = range.getY(); 
+            sr.offsetIdx2 = 6;
+            sr.row = 0; sr.n = n1; sr.nMax = n1; 
+            sr.diffChordSum = 1; sr.diffChordSum = 10;
+            if (range.getX() == 4 && range.getY() == 6) {
+                sr.offsetIdx2 = 6;
+            } else if (range.getX() == 9 && range.getY() == 11) {
+                sr.offsetIdx2 = 4;
+            }
+            intervals.add(sr);
+        }
+        
+        // add a bad interval last that overlaps the others and should
+        //  not be successfullt added to the tree
+        SR sr = new SR();
+        sr.startIdx1 = 6; sr.stopIdx1 = 9; 
+        sr.offsetIdx2 = 0;
+        sr.row = 0; sr.n = n1; sr.nMax = n1; 
+        sr.diffChordSum = 1; sr.diffChordSum = 10;
+        intervals.add(sr);
+        
+        OrderedClosedCurveCorrespondence occ = new 
+            OrderedClosedCurveCorrespondence();
+        occ.addIntervals(intervals, n1, n2);
+        
+        List<SR> results = occ.getResultsAsList();
+        
+        assertEquals(3, results.size());
+        
+    }
 }
