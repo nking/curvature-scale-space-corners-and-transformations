0b0ba13cfa485dfb6a04c2c944f88405a5efa36d
nichole <nichole@climbwithyourfeet.com>
2017-01-26 08:20:02 -0800

    adding more to HOGs.java

--- src/algorithms/imageProcessing/features/GradientIntegralHistograms.java
+++ src/algorithms/imageProcessing/features/GradientIntegralHistograms.java
@@ -11,9 +11,9 @@ import java.util.Arrays;
  Algorithm uses a pattern similar to a summed area table
  to cumulatively add the content from one lower pixel in
  x and in y with a runtime complexity that is O(N_pixels),
- * but a space complexity that is O(N_pixels * nBins) where
- * nBins is the number of bins in a histogram (default is 16 for
- * default value range of 0 to 255, inclusive).
+ but a space complexity that is O(N_pixels * nBins) where
+ nBins is the number of bins in a histogram (default is 16 for
+ default value range of 0 to 255, inclusive).
  
  * @author nichole
  */
