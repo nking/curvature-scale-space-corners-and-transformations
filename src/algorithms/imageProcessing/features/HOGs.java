0734d11665d62ebce83cbfdcc72afb5a92780296
nichole <nichole@climbwithyourfeet.com>
2017-01-26 02:30:36 -0800

    adding more notes to HOGs.java

--- src/algorithms/imageProcessing/features/HOGs.java
+++ src/algorithms/imageProcessing/features/HOGs.java
@@ -83,11 +83,12 @@ package algorithms.imageProcessing.features;
   NOTEs about color spaces:
      The normalization above assumes that the greyscale image is generated
      from RGB.
-     For the CIE LIV polar theta greyscale, one would not want to normalize
-     the histograms because the illumination has already been handled in 
-     the polar angle of the chromaticity conversion to greyscale.
+     For the CIE LUV polar theta greyscale image, one would not want to 
+     normalize the histograms because the illumination has already been 
+     handled in the polar angle of the chromaticity conversion to greyscale.
      so a method is needed to extract unnormalized histograms
-     for a block too.
+     for a block too and the intersection calculation then is different
+     also...havne't thought this through yet.
      
   
  * @author nichole
