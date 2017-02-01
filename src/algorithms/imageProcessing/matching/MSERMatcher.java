62fa0dc3115d31f0686a3f25fabd9989d6b1f951
nichole <nichole@climbwithyourfeet.com>
2017-02-01 09:40:49 -0800

    small adjustments to cell size in mser matching method

--- src/algorithms/imageProcessing/matching/MSERMatcher.java
+++ src/algorithms/imageProcessing/matching/MSERMatcher.java
@@ -1077,8 +1077,8 @@ public class MSERMatcher {
         TIntObjectMap<HGS> hgsMap1 = new TIntObjectHashMap<HGS>();
 
         // 16, 12
-        int nPixPerCellDimH = 6;;
-        int nPixPerCellDim = 4;//6;
+        int nPixPerCellDimH = 6;
+        int nPixPerCellDim = 4;
         
         // use hogs to calculate the dominant orientations
         calculateDominantOrientations(regionPoints0, 
