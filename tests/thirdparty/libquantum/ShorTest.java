a41e11b8583126f63dc9daf6ae13f50e5da4419a
nichole <nichole@climbwithyourfeet.com>
2017-05-11 22:19:01 -0700

    starting unit tests for Shor's algorithm

---
+++ tests/thirdparty/libquantum/ShorTest.java
@@ -0,0 +1,43 @@
+package thirdparty.libquantum;
+
+import java.util.Arrays;
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nichole
+ */
+public class ShorTest extends TestCase {
+    
+    public ShorTest() {
+    }
+    
+    public void testRun() {
+        
+        int number = 15;
+        
+        final int[] expected = new int[]{3, 5};
+        
+        int nTests = 50;
+        int nCorrect = 0;
+        
+        for (int i = 0; i < nTests; ++i) {
+            
+            Shor shor = new Shor(15);
+        
+            int[] factors = shor.run();
+            
+            if (factors != null && factors.length == 2) {
+                Arrays.sort(factors);
+                if (Arrays.equals(expected, factors)) {
+                    nCorrect++;
+                }
+            } 
+        }
+        
+        System.out.println("nTests=" + nTests + " nCorect=" + nCorrect);
+        
+    }
+}
