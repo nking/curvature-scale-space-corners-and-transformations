cb0c9045dc8da4aaa124ac448497c6361744304a
nichole <nichole@climbwithyourfeet.com>
2018-04-24 19:07:16 -0700

    stub for class to find cell centers for HOG detector windows which have irregular bounds.

---
+++ src/packing/Intersection2DPacking.java
@@ -0,0 +1,35 @@
+package packing;
+
+/**
+ * Class to return a set of points for the intersection of 2 point sets where 
+ * the set of points are separated by a given interval in x and y, that is,
+ * the bins are squares.
+ * The two point sets can be irregular in shape.  
+ * The solution is not guaranteed to be optimal.
+ * 
+ * The runtime complexity is expected to be:
+ *     O(min(N_points) * lg_2(N_points))
+ * 
+ * Considering implementing these:
+ * 
+ * Option 1:
+ *     resembles 2D strips:
+ *     -- find the intersection of the point sets by iterating over the smallest
+ *        point set.
+ *     -- sort the intersection data structure by x, then y
+ *     -- fill the intersection space with rectangles of given x and y size.
+ *        -- use IntervaleRangeSearch for collision checks
+ *        
+ * Option 2:
+ *     uses a medial axis built from a voronoi diagram
+ *     (see algorithms.compGeometry.MedialAxis), 
+ *     then fills the space using order based upon the most connected medial 
+ *     axis points and then the space connected to those assigned bins, 
+ *     iteratively.
+ *     It also uses IntervaleRangeSearch for collision checks.
+ * 
+ * @author nichole
+ */
+public class Intersection2DPacking {
+    
+}
