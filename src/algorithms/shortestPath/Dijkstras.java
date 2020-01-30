74cbde180dce50250e505dc79fe21a6563029c04
nichole <nichole@climbwithyourfeet.com>
2020-01-29 16:17:38 -0800

    adding dijkstras single source paths, needed for jphnson algorithm.

---
+++ src/algorithms/shortestPath/Dijkstras.java
@@ -0,0 +1,290 @@
+package algorithms.shortestPath;
+
+import algorithms.bipartite.MinHeapForRT2012;
+import algorithms.imageProcessing.HeapNode;
+import algorithms.misc.MiscMath;
+import algorithms.util.SimpleLinkedListNode;
+import gnu.trove.iterator.TIntIntIterator;
+import gnu.trove.map.TIntIntMap;
+import java.util.Arrays;
+
+/**
+ * given a weighted directed graph with weight function, the shortest path from
+ * u to v is the path whose sum of weights is the minimum.
+ * 
+ * All edge weights must be non-negative.
+ * 
+ * The runtime complexity is O(V + E) due to use of a YFastTrie as the min-priority heap.
+ * Note that if the heap wrapper has to choose a Fibonacci instead due to
+ * memory constraints, the runtime complexity is O(V*log_2V + E) instead.
+ * 
+ * implemented from pseudocode from Cormen et al. "Introduction to Algorithms".
+ * 
+ * from Cormen et al. :
+ * 
+ * @author nichole
+ */
+public class Dijkstras {
+    
+    /**
+     * directed acyclic graph where the main index of the object array
+     * is the vertex number, and each vertex has a linked list of outgoing 
+     * edges connecting to the vertexes held in each linked list node key.
+     */
+    protected SimpleLinkedListNode[] g = null;
+    
+    /* edge weights in format of outer array index being the
+     * first vertex and the map within having a key being the 2nd vertex of the
+     * edge with value being the edge weight.
+    */
+    protected TIntIntMap[] w = null;
+    
+    protected int[] dist = null;
+
+    protected int[] predecessor = null;   
+    
+    protected int src = -1;
+    
+    private int sentinel = Integer.MAX_VALUE;
+    
+    // this is recalculated in constructor
+    private int maxValue = sentinel - 1;
+
+    // key is total estimate from srcIdx to destIdx for the given refIdx
+    //    (that is the distance from srcIdx to refIdx + refIdx + heuristic)
+    protected MinHeapForRT2012 heap = null;
+
+    // refs to nodes internal to heap for decrease key operations
+    protected HeapNode[] nodes = null;
+    
+    public Dijkstras() {
+    }
+    
+    /**
+     * find the single shortest path in dAG with edge weights w starting from s.
+     * @param dAG directed acyclic graph where the main index of the object array
+     * is the vertex number, and each vertex has a linked list of outgoing 
+     * edges connecting to the vertexes held in each linked list node key.
+     * Note that all vertexes, including edge vertexes, must be present as an
+     * index of the array dAG, i.e. all vertexes must have numerical value 
+     * less than dAG.length.
+     * @param weights the edge weights in format of outer array index being the
+     * first vertex and the map within having a key being the 2nd vertex of the
+     * edge with value being the edge weight.
+     * @param sourceVertex the source vertex index
+     */
+    public void find(SimpleLinkedListNode[] dAG, TIntIntMap[] weights, int sourceVertex) {
+        
+        if (dAG == null || dAG.length == 0) {
+            throw new IllegalArgumentException("dAG cannot be null");
+        }
+        if (sourceVertex < 0 || sourceVertex >= dAG.length) {
+            throw new IllegalArgumentException("sourceIndex cannot be null");
+        }
+        
+        /*
+        initialize single source (g, s)
+        s=0
+        add vertexes to heap
+        while heap is not empty
+          do u = extract-min
+            s = s union u
+            for each vertex v in adj[u]
+               do relax(u, v, w)
+        */ 
+        
+        init(dAG, weights, sourceVertex);
+        
+        find();
+    }
+        
+    private void find() {
+                               
+        while (heap.getNumberOfNodes() > 0) {
+
+            HeapNode uNode = heap.extractMin();
+            
+            int u = ((Integer)uNode.getData()).intValue();
+            
+            TIntIntMap uWeights = w[u];
+            
+            // null the entry in nodes so it isn't used in decrease key
+            nodes[u] = null;
+            
+            if (uWeights == null) {
+                continue;
+            }
+            
+            SimpleLinkedListNode next = g[u];
+            
+            while (next != null && next.getKey() != -1) {
+            
+                int v = next.getKey();
+                
+                if (!uWeights.containsKey(v)) {
+                    throw new IllegalStateException("no weight found for edge " 
+                    + u + " to " + v);
+                }
+                int wUV = uWeights.get(v);
+            
+                int dUPlusWUV = wUV;
+                if (dist[u] == sentinel) {
+                    dUPlusWUV = sentinel;
+                } else {
+                    dUPlusWUV += dist[u];
+                }
+                                
+                if (dist[v] > dUPlusWUV) {
+                    dist[v] = dUPlusWUV;
+                    predecessor[v] = u;
+                }
+                
+                next = next.getNext();
+            }
+        }
+    }
+    
+    private void init(SimpleLinkedListNode[] dAG, TIntIntMap[] weights, int sourceVertex) {
+        
+        g = dAG.clone();
+        for (int i = 0; i < g.length; ++i) {
+            g[i] = new SimpleLinkedListNode(g[i]);
+        }
+        w = Arrays.copyOf(weights, weights.length);
+        src = sourceVertex;
+        
+        maxValue = calcUpperLimitKeyValue();
+        
+        sentinel = maxValue + 1;
+    
+        dist = new int[g.length];
+        predecessor = new int[g.length];
+        
+        Arrays.fill(dist, sentinel);
+        Arrays.fill(predecessor, -1);
+        
+        dist[src] = 0;
+                
+        initHeap();
+    }
+    
+    private void initHeap() {
+        
+        int n = g.length;
+                
+        int nBits = (int)Math.ceil(Math.log(maxValue/Math.log(2)));
+        
+        //int maxValue, int approxN, int maxNumberOfBits
+        heap = new MinHeapForRT2012(sentinel, n, nBits);
+
+        nodes = new HeapNode[n];
+
+        // initialize all except the source node as having infinite distance
+        for (int i = 0; i < g.length; ++i) {
+
+            if (g[i] == null) {
+                continue;
+            }
+            
+            int key = dist[i];
+
+            HeapNode node = new HeapNode(key);
+            node.setData(Integer.valueOf(i));
+
+            heap.insert(node);
+
+            nodes[i] = node;
+        }            
+    }
+    
+    /**
+     * get shortest path from source to destIndex
+     * @param destVertex
+     * @return 
+     */
+    public int[] getShortestPathToVertex(int destVertex) {
+        if (destVertex < 0 || destVertex >= g.length) {
+            throw new IllegalArgumentException("destIndex cannot be null");
+        }
+        if (predecessor == null) {
+            throw new IllegalStateException("find must be run before this method can be used");
+        }
+        
+        int[] p = new int[g.length];
+        p[p.length - 1] = destVertex;
+                
+        for (int i = p.length - 2; i > -1; --i) {
+            if (destVertex == src) {
+                int len = p.length - 1 - i;
+                int[] t = new int[len];
+                System.arraycopy(p, i + 1, t, 0, len);
+                return t;
+            } else if (destVertex == -1) {
+                throw new IllegalStateException("path did not complete correctly");
+            }
+            p[i] = predecessor[destVertex];
+            destVertex = p[i];
+        }
+        
+        if (p[0] != src) {
+            throw new IllegalStateException("path did not complete correctly for destIndex");
+        }
+        
+        return p;
+    }
+    
+    public int getSumOfPath(int[] vertexes) {
+        if (vertexes == null) {
+            throw new IllegalArgumentException("vertexes cannot be null");
+        }
+        if (dist == null) {
+            throw new IllegalStateException("find must be run before this method can be used");
+        }
+        int sum = 0;
+        int u, v;
+        for (int i = 1; i < vertexes.length; ++i) {
+            u = vertexes[i - 1];
+            v = vertexes[i];
+            
+            TIntIntMap uWeights = w[u];
+            
+            if (!uWeights.containsKey(v)) {
+                throw new IllegalStateException("no weight found for edge " 
+                    + u + " to " + v);
+            }
+            
+            sum += uWeights.get(v);
+        }
+        return sum;
+    }
+
+    private int calcUpperLimitKeyValue() {
+        
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < w.length; ++i) {
+            TIntIntMap map = w[i];
+            TIntIntIterator iter = map.iterator();
+            int value;
+            for (int j = 0; j < map.size(); ++j) {
+                iter.advance();
+                value = iter.value();
+                if (value > max) {
+                    max = value;
+                }
+            }
+        }
+        
+        // if # of bits in max * g.length is larger than max_integer,
+        //   use max_integer, else use it
+        int b1 = MiscMath.numberOfBits(max);
+        int b2 = MiscMath.numberOfBits(g.length);
+        if ((b1 + b2) > 31) {
+            max = Integer.MAX_VALUE - 1;
+        } else {
+            max *= g.length;
+        }
+        
+        return max;
+    }
+    
+}
---
+++ tests/algorithms/shortestPath/DijkstrasTest.java
@@ -0,0 +1,91 @@
+package algorithms.shortestPath;
+
+import algorithms.util.SimpleLinkedListNode;
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import java.util.Arrays;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author nichole
+ */
+public class DijkstrasTest extends TestCase {
+    
+    public DijkstrasTest() {
+    }
+    
+    public void test0() {
+        /*
+        from Cormen et al. Fig 24.6
+           edges   weight
+             s t     10
+             s y     5
+             t x     1
+             t y     2
+             x z     4
+             y t     3
+             y x     9
+             y z     2
+             z s     7
+             z x     4
+        
+           s  t  x  y  z
+           0  1  2  3  4
+        */
+        SimpleLinkedListNode[] g = new SimpleLinkedListNode[5];
+         for (int i = 0; i < g.length; ++i) {
+             g[i] = new SimpleLinkedListNode();
+         }
+         g[0].insert(1); g[0].insert(3);
+         g[1].insert(2); g[1].insert(3);
+         g[2].insert(4);
+         g[3].insert(1); g[3].insert(2); g[3].insert(4);
+         g[4].insert(0); g[4].insert(2);
+         TIntIntMap[] w = new TIntIntMap[5];
+         w[0] = new TIntIntHashMap(2); w[0].put(1, 10); w[0].put(3, 5);
+         w[1] = new TIntIntHashMap(2); w[1].put(2, 1); w[1].put(3, 2);
+         w[2] = new TIntIntHashMap(1); w[2].put(4, 4);
+         w[3] = new TIntIntHashMap(3); w[3].put(1, 3); w[3].put(2, 9); w[3].put(4, 2);
+         w[4] = new TIntIntHashMap(2); w[4].put(0, 7); w[4].put(2, 4);
+         
+         Dijkstras dijkstras = new Dijkstras();
+         dijkstras.find(g, w, 0);
+         
+         int[] p;
+         int dist, dest;
+         // s  t  x  y  z
+         // 0  1  2  3  4
+         
+         dest = 3;
+         p = dijkstras.getShortestPathToVertex(dest);
+         assertTrue(Arrays.equals(new int[]{0, 3}, p));
+         dist = dijkstras.getSumOfPath(p);
+         assertEquals(5, dist);
+         
+         dest = 1;
+         p = dijkstras.getShortestPathToVertex(dest);
+         assertTrue(Arrays.equals(new int[]{0, 3, 1}, p));
+         dist = dijkstras.getSumOfPath(p);
+         assertEquals(8, dist);
+         
+         dest = 2;
+         p = dijkstras.getShortestPathToVertex(dest);
+         assertTrue(Arrays.equals(new int[]{0, 3, 1, 2}, p));
+         dist = dijkstras.getSumOfPath(p);
+         assertEquals(9, dist);
+         
+         dest = 4;
+         p = dijkstras.getShortestPathToVertex(dest);
+         assertTrue(Arrays.equals(new int[]{0, 3, 4}, p));
+         dist = dijkstras.getSumOfPath(p);
+         assertEquals(7, dist);
+         
+         dest = 3;
+         p = dijkstras.getShortestPathToVertex(dest);
+         dist = dijkstras.getSumOfPath(p);
+         assertEquals(5, dist);
+         
+    }
+    
+}
