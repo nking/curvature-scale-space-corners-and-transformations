34074a090e27f150a93d42b6dd38ba2212905028
nichole <nichole@climbwithyourfeet.com>
2021-06-18 18:10:21 -0700

    adding a couple partial derivatives to BundleAdjustment.java

--- src/algorithms/imageProcessing/transform/BundleAdjustment.java
+++ src/algorithms/imageProcessing/transform/BundleAdjustment.java
@@ -38,4 +38,81 @@ quaternion algebra", Acta Astronautica (2010), doi:10.1016/j.actaastro.2010.06.0
  */
 public class BundleAdjustment {
     
+    /**
+     * the partial derivative of the
+     * final 2D re-projected coordinates of the i-th feature point
+     * w.r.t. 2D coordinates of perspective projection of the i-th feature point.
+     * Defined in Qu 2018 eqn (3.12).
+     * 
+     * @param xWCNI a world point projected to the camera reference frame and
+     * normalized by it's last coordinate.
+     * xWCI = column i of coordsW transformed to camera coordinates; 
+     * xWCNI = xWCI/xWCI[2];
+     * @param intr
+     * @param k1 radial distortion coefficient 1
+     * @param k2 radial distortion coefficient 2
+     * @param rot extrinsic camera parameter rotation matrix.
+     * @param trans extrinsic camera parameter translation vector.
+     * @param out output array of size [2X2]
+     */
+    static void pdCpIJCIJ(double[] xWCNI, Camera.CameraIntrinsicParameters intr,
+        double k1, double k2, 
+        double[][] rot, double[] trans,
+        double[][] out) {
+        
+        if (out.length != 2 || out[0].length != 2) {
+            throw new IllegalArgumentException("out size must be 2X2");
+        }
+        
+        double x = -xWCNI[0];
+        double y = -xWCNI[1];
+        
+        double x2 = x*x;
+        double x4 = x2*x2;
+        double y2 = y*y;
+        double y4 = y2*y2;
+        
+        double f1 = intr.getIntrinsic()[0][0];
+        
+        double pdxx = f1 * (1 + k1*(3*x2 + y2) + k2*(5*x4 + y4 + 6*x2*y2));
+        double pdxy = 2*f1*x*y*(k1 + 2*k2*(x2 + y2));
+        double pdyx = pdxy;
+        double pdyy = f1*(k1*(3*y2 + x2) + k2*(5*y4 + x4 + 6*x2*y2));
+        
+        out[0][0] = pdxx;
+        out[0][1] = pdxy;
+        out[1][0] = pdyx;
+        out[1][1] = pdyy;
+    }
+    
+    /**
+     * the partial derivative of the 2D coordinates of perspective projection 
+     * of the i-th feature point normalized, w.r.t. to the same notnormalized.
+     * Defined in Qu 2018 eqn (3.16).
+     * 
+     * @param xWCI a world point projected to the camera reference frame.
+     * xWCI = column i of coordsW transformed to camera coordinates; 
+     * @out output array of size [2X3]
+     */
+    static void pdCIJXWIJ(double[] xWCI, double[][] out) {
+        
+        if (out.length != 2 || out[0].length != 3) {
+            throw new IllegalArgumentException("out size must be 2X3");
+        }
+        
+        double x = xWCI[0];
+        double y = xWCI[1];
+        double z = xWCI[2];
+        double z2 = z*z;
+        
+        out[0][0] = -1./z;
+        out[0][1] = 0;
+        out[0][2] = x/z2;
+        
+        out[1][0] = 0;
+        out[1][1] = -1./z;
+        out[1][2] = y/z2;
+        
+    }
+    
 }
