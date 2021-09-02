nking/curvature-scale-space-corners-and-transformations

commit 92e9b2512e9c13b7258c30a553d09ebd422b93a9

removing unfinished ransac algorithm methods

src/algorithms/imageProcessing/features/RANSACEuclideanSolver.java
@@ -61,39 +61,18 @@ public class RANSACEuclideanSolver {
             + matchedLeftXY.getN());
         }
         
-        /*
-        -- the number of iterations for testing sub-samples of nPoints 
-           (each of size 2) and finding one to be a good sub-sample with an
-           excess of probability of 95% is estimated for a given
-           percent of bad data.
-           NOTE, the algorithm proceeds by assuming 50% bad data and improves
-           that upon each best fitting sub-sample.
-        -- for each iteration of solving epipolar transformation using a sample
-           of size 2, the resulting transformation is evaluated on the
-           all points of the dataset.
-           If the number of inliers is T or more, the fit is re-done with all
-           of the points, where 
-           T = (1. - outlierPercentage) * (total number of data points)
-           The best fitting for all iterations as defined by number of inliers 
-           and standard deviation from an epipolar line, is kept each time.
-        -- at the end of each iteration, the number of iterations is then 
-           re-calculated if it can be reduced.
-        
-        NOTE that the sub-samples are selected randomly from all possible
-        sub-samples of the nPoints unless the number of all possible 
-        sub-samples is smaller than the expected number of iterations for 95%
-        probability of a good sub-sample.  In the later case, all sub-samples
-        are tried.
-        
-        */
-
         final int nSet = 2;
 
         final int nPoints = matchedLeftXY.getN();
                 
         // n!/(k!*(n-k)!
-        final long nPointsSubsets = MiscMath.computeNDivKTimesNMinusK(
+        long nPointsSubsets;
+        try {
+            nPointsSubsets = MiscMath.computeNDivKTimesNMinusK(
             nPoints, nSet);
+        } catch (java.lang.ArithmeticException ex) {
+            nPointsSubsets = Long.MAX_VALUE;
+        }
         boolean useAllSubsets = false;
         
         SecureRandom sr = Misc.getSecureRandom();
@@ -116,22 +95,18 @@ public class RANSACEuclideanSolver {
         
         int outlierPercent = 50;
         int t = (int)Math.ceil((1. - outlierPercent)*nPoints);
-        
+      
         long nMaxIter;
         if (nPoints == nSet) {
             nMaxIter = 1;
             useAllSubsets = true;
         } else {
             nMaxIter = RANSACAlgorithmIterations
-                .numberOfSubsamplesFor95PercentInliers(outlierPercent, nSet);
+                .numberOfSubsamplesOfSize7For95PercentInliers(outlierPercent);
         }
         
-        RANSACAlgorithmIterations nEstimator = new RANSACAlgorithmIterations();
-        long nMaxIter2 = nEstimator.estimateNIterFor99PercentConfidence(nPoints, 2, 0.5);
-        
         System.out.println("nPoints=" + nPoints + " estimate for nMaxIter=" +
-            nMaxIter + " (n!/(k!*(n-k)!)=" + nPointsSubsets
-            + " nMaxIter2=" + nMaxIter2);
+            nMaxIter + " (n!/(k!*(n-k)!)=" + nPointsSubsets);
 
         if (nMaxIter > nPointsSubsets) {
             nMaxIter = nPointsSubsets;

src/algorithms/imageProcessing/util/RANSACAlgorithmIterations.java
@@ -71,200 +71,6 @@ simplified below.
  * @author nichole
  */
 public class RANSACAlgorithmIterations {
-
-    public long estimateNIterFor99PercentConfidence(
-        int nPoints, int sampleSize, double expectedFractionTruePoints) {
-        
-        if (sampleSize < 1) {
-            throw new IllegalArgumentException("sampleSize must be > 0");
-        }
-        if (nPoints < 1 || (nPoints > 1789)) {
-            throw new IllegalArgumentException("nPoints must be a positive number < 1790");
-        }
-        if (expectedFractionTruePoints < 0.0000001 || (expectedFractionTruePoints > 1.0)) {
-            throw new IllegalArgumentException(
-            "expectedFractionTruePoints must be larger than 0 and less than 1");
-        }
-        
-        double pSample = calculateTrueSampleProbability(nPoints, sampleSize, 
-            expectedFractionTruePoints);
-        
-        // pSample must be larger than approx 1e-16 and <= 0.99
-        
-        long nIter = Math.round(Math.log(1. - 0.99) / Math.log(1. - pSample)) + 1;
-           
-        return nIter;
-    }
-    
-    public long estimateNIterFor99PercentConfidenceDegenerate(
-        int nPoints, int nPointsIncludingDegenerate, int sampleSize, 
-        double expectedFractionTruePoints) {
-        
-        if (sampleSize < 1) {
-            throw new IllegalArgumentException("sampleSize must be > 0");
-        }
-        if (nPoints < 1 || (nPoints > 1789) ) {
-            throw new IllegalArgumentException("nPoints must be a positive " +
-            " number < 1790 or even smaller considering multiplicity");
-        }
-        if (expectedFractionTruePoints < 0.0000001 || expectedFractionTruePoints > 1.0) {
-            throw new IllegalArgumentException(
-            "expectedFractionTruePoints must be larger than 0 and less than 1");
-        }
-        if (nPoints <= sampleSize) {
-            throw new IllegalArgumentException("nPoints must be larger than sampleSize");
-        }
-        
-        double pSample = calculateTrueSampleProbabilityForMultiplyMatched(nPoints, 
-            nPointsIncludingDegenerate, sampleSize, expectedFractionTruePoints);
-
-        long nIter;
-        
-        if (pSample == 0) {
-            return Long.MAX_VALUE;
-        } else if (pSample < 1E-16) {
-            // return naive estimate
-            return Math.round(1./pSample);
-        } else {
-            nIter = Math.round(Math.log(1. - 0.99) / Math.log(1. - pSample)) + 1;
-        }
-        
-        return nIter;
-    }
-    
-    /**
-     * calculate the probability of drawing an all 'T' sample of sampleSize 
-     * points from a universe of nPoints of which 
-     * (expectedFractionTruePoints*nPoints) are 'T'.  
-     * The calculation is drawing each point without replacement,
-     * that is (nTruePts/nPts) * ((nTrue-1)/(nPoints-1)) * ((nTrue-2)/(nPoints-2)) ...
-     * 
-     * If nTruePts is less than sampleSize (due to small expectedFractionTruePoints
-     * for example), the calculation becomes 
-     * @param nPoints
-     * @param sampleSize
-     * @param expectedFractionTruePoints
-     * @return 
-     */
-    public double calculateTrueSampleProbability(int nPoints, int sampleSize,
-        double expectedFractionTruePoints) {
-        
-        if (sampleSize < 1) {
-            throw new IllegalArgumentException("sampleSize must be > 0");
-        }
-        if (nPoints < 1 || (nPoints > 1789) ) {
-            throw new IllegalArgumentException("nPoints must be a positive " 
-                + " number < 1790 or even smaller considering multiplicity");
-        }
-        if (expectedFractionTruePoints < 0.0000001 || expectedFractionTruePoints > 1.0) {
-            throw new IllegalArgumentException(
-            "expectedFractionTruePoints must be larger than 0 and less than 1");
-        }
-        if (nPoints < sampleSize) {
-            throw new IllegalArgumentException("nPoints must be larger than sampleSize");
-        }
-        
-        double nTruePoints = nPoints * expectedFractionTruePoints;
-        
-        double factor = 1;
-        
-        if (nTruePoints >= sampleSize) {        
-            for (int i = 0; i < sampleSize; ++i) {
-                factor *= (nTruePoints - i)/(double)(nPoints - i);
-            }            
-        } else {
-            //know that the number is still smaller than (1./nPoints)^sampleSize.
-            //For nTruePoints of 1, looks like (1./nPoints)*(1./(nPoints-sampleSize))^(sampleSize-1)
-            for (int i = 0; i < (int)nTruePoints; ++i) {
-                factor *= (nTruePoints - i)/(double)(nPoints - i);
-            }
-            if (nPoints != sampleSize) {
-                int pow = (sampleSize - (int)nTruePoints);
-                factor *= Math.pow((1./(nPoints - sampleSize)), pow);
-            }
-        }
-        
-        return factor;
-    }
-    
-    public double calculateTrueSampleProbabilityForMultiplyMatched(
-        int nPoints, int nAllMultiplicity, int sampleSize, 
-        double expectedFractionTruePoints) {
-        
-        /* 
-        making an assumption of avg multiplicity for all and using the same
-        logic as for singly matched points ts start, then adding the multiple
-        matching logic:
-        
-        example:  5 points in set 1 and each is singly matched to a point in set 2.
-                  sample size, k, is 2.
-                  there are 3 true point 1's {0, 2, 3}
-        
-        the number of combinations of the singly matched dataset are n!/(k!(n-k)!)
-        nCombinations=10. 
-        pSample (as seen in CountingTest) is the factors of nTrueMatches/nPoints
-        reduced for each subsequent point due to drawing without replacement.
-        pSample=(3./5)*(2./4)=0.3 
-        The expected number of 'true' samples, that is samples composed of only
-        truely matched points is then pSample*nCombinations = 0.3*10 = 3.
-        nTrueSamples=3 is verified as seen here:
-        0 1
-        0 2 * 
-        0 3 *
-        0 4
-        1 2
-        1 3
-        1 4
-        2 3 *
-        2 4
-        3 4
-        
-        For multiply matched points, the number of combinations increases,
-        but the number of truly matched remains the same.
-        
-        The number of all point pairs is the multiplicity * nPoints
-        (or alternatively, can be given the total number including multiplicity).
-      
-        Using the sample example, adding a multiplicity of '2' to each point,
-        the number of all point pairs is 2 * nPoints = 10.
-        the number of possible combinations is 10*9./2 = 45.
-        The number of true samples hasn't changed and that is still 3.
-        The pSample then must be 3./45. = 0.067.
-        so the multiply mapped pSample is pSample_singly_matched*nComb_singlyMatched/nComb_multiplyMatched
-          
-        in detail:
-        
-        The number of combinations for the multiply matched is 
-            nMultiplyMatched!/(k!(nMultiplyMatched-k)!)
-        
-        The multiply mapped pSample is
-            pSample_singly_matched * nComb_singlyMatched / nComb_multiplyMatched
-        
-        For the example above,
-            pSample_multiply_matched = 0.3 * (10./45.) = 0.067
-        
-        A better determination would use the real integrated CDF of the
-        inhomogeneous distribution, but this number is used to quickly 
-        make an estimate.
-        
-        Using the maximum multiplicity would lead to a safe overestimate,
-        so should consider that for another method.
-        */
-        
-        double pSampleSingle = calculateTrueSampleProbability(nPoints, sampleSize, 
-            expectedFractionTruePoints);
-        
-        double nCombinationsSingle = MiscMath.computeNDivKTimesNMinusK(nPoints, sampleSize);
-        
-        double nCombinationsMultiple = 
-            MiscMath.computeNDivKTimesNMinusK(nAllMultiplicity, sampleSize);
-        
-        double pSampleMultiple = pSampleSingle * 
-            (nCombinationsSingle/nCombinationsMultiple);
-
-        return pSampleMultiple;
-    }
-
     
     /*
     Get the number of subsamples needed for a subsample size of 7 within a larger sample,

commit 9ac928f873167e4a5b604630fd1fc0709e2656cc

correcting an error introduced in commit 92e9b2512e9c13b7258c30a553d09ebd422b93a9

src/algorithms/imageProcessing/features/RANSACEuclideanSolver.java
@@ -102,7 +102,8 @@ public class RANSACEuclideanSolver {
             useAllSubsets = true;
         } else {
             nMaxIter = RANSACAlgorithmIterations
-                .numberOfSubsamplesOfSize7For95PercentInliers(outlierPercent);
+                .numberOfSubsamplesFor95PercentInliers(outlierPercent, nSet);
+                //.numberOfSubsamplesOfSize7For95PercentInliers(outlierPercent);
         }
         
         System.out.println("nPoints=" + nPoints + " estimate for nMaxIter=" +

src/algorithms/imageProcessing/util/RANSACAlgorithmIterations.java
@@ -101,29 +101,7 @@ public class RANSACAlgorithmIterations {
     </pre>
     */
     public static int numberOfSubsamplesOfSize7For95PercentInliers(int outlierPercent) {
-    
-        if (outlierPercent < 0) {
-            throw new IllegalArgumentException("outlierPercent must be non-negative");
-        }
-        if (outlierPercent > 50) {
-            throw new IllegalArgumentException("outlierPercent must be 50 percent or less");
-        }
-        
-        /*
-        1 - gamma = (1 - (1 - eps)^p)^m
-           let Z = (1 - (1 - eps)^p)
-           let g = 1 - gamma
-        g = Z^m
-        log(g) = m*log(Z);       
-        m = log(g) / log(Z)
-        */
-        int p = 7;
-        double z = 1. - Math.pow(1. - ((double)outlierPercent/100.), p);
-        double g = 1. - 0.95;
-        
-        double m = Math.log(g) / Math.log(z);
-                
-        return (int)Math.ceil(m);
+        return numberOfSubsamplesFor95PercentInliers(outlierPercent, 7);
     }
     
     /*