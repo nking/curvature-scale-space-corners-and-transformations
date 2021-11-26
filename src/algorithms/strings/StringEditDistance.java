6d0e10a63d0af8e9d9bf5362d5c4de87cebe5e25
nichole <nichole@climbwithyourfeet.com>
2021-11-26 15:25:57 -0800

    updating shared jars and files moved into the shared project. correcting the output indexes EditDistance.java

--- src/algorithms/strings/StringEditDistance.java
+++ src/algorithms/strings/StringEditDistance.java
@@ -1,8 +1,6 @@
 package algorithms.strings;
 
 import algorithms.util.PairIntArray;
-import gnu.trove.list.TIntList;
-import java.util.Arrays;
 
 /**
  * find the number of insert, delete, and substitution operations to change
@@ -34,12 +32,11 @@ public class StringEditDistance {
      * @param outIndexes a and b indexes of the solution in pairs (a_i, b_j)
      * @return 
      */
-    public int calculateWithWagnerFischer(String a, String b,
-        PairIntArray outIndexes) {
+    public int calculateWithWagnerFischer(String a, String b, PairIntArray outIndexes) {
         
         final int cDel = 1;
         final int cIns = 1;
-
+        
         int cChangeUnequal = 1;
         int m = a.length();
         int n = b.length();
@@ -118,12 +115,152 @@ public class StringEditDistance {
             } else if (d[i][j] == d[i][j-1] + cIns) {
                 j--;
             } else {
-                outIndexes.add(i, j);
+                outIndexes.add(i-1, j-1);// subtracting 1 for 0-based indexes
                 i--;
                 j--;
             }
         }
+        //System.out.printf("m=%d, n=%d, d=\n%s\n", m, n, FormatArray.toString(d, "%d"));
         return d[m][n];
+    }
+    
+    /**
+     * calculate the number of insert, delete, and substitution operations to change
+     * one string into another following Wagner and Fischer 1954 
+     * "The String-to-String Correction Problem", but modified to reduce the space complexity.
+     * This uses dynamic programming with time O(a.length * b.length)
+     * and space O(a.length).
+     * <pre>
+     * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.367.5281&rep=rep1&type=pdf
+     * 
+     * for space complexity reduction, lecture slides of
+     * CS 473: Fundamental Algorithms, Spring 2011
+     https://courses.engr.illinois.edu/cs473/sp2011/Lectures/09_lec.pdf
+     Slide Acknowledgments: Lecture notes are based on notes of Jeff Erickson, 
+     Sariel Har-Peled, Chandra Chekuri and the slides of Mahesh Viswanathan and 
+     the books by Kleinberg and Tardos, and by Dasgupta, Papadimitrioiu and Vazirani.
+
+     * Also, see Levenshtein Distance:
+     * https://en.wikipedia.org/wiki/Levenshtein_distance
+     * </pre>
+     * @param a string to edit
+     * @param b target string to change a into.
+     * 
+     * @return the edit distance which is the minimum number of deletes of 
+     * characters in string a and inserts of characters into string b to turn
+     * string a into string b.
+     */
+    public int calculateWithModifiedWagnerFischer(String a, String b) {
+       
+        final int cDel = 1;
+        final int cIns = 1;
+       
+        /*
+            Entries in jth column only depend on (j − 1)’s column and earlier 
+               entries in jth column.
+            Only store the current column and the previous column reusing space; 
+               N(i, 0) stores M(i, j − 1) and N(i, 1) stores M(i, j)
+        */
+        
+        int cChangeUnequal = 1;
+        int m = a.length();
+        int n = b.length();
+        int[][] dN = new int[m + 1][];
+        int i;
+        //dN(i, 0) stores M(i, j − 1) and dN(i, 1) stores M(i, j)
+        for (i = 0; i <= m; ++i) {
+            dN[i] = new int[2];
+        }
+        // (m+1) X (n+1)
+        /*int[][] d = new int[m + 1][];
+        for (int i = 0; i <= m; ++i) {
+            d[i] = new int[n + 1];
+        }*/
+        
+        //Theorem 2
+        //D (i, j) is the cost of the least cost trace from A (i) to B (j).
+        //D(i, j) = min( 
+        //               D(i - 1, j - 1) + cost(A(i) -> B(j)), 
+        //               D(i- 1,j) + cost(A(i) deleted),
+        //               D(i,j -- 1) + cost(insert a character that is  B(j}) )
+        //          for all i, j, where 1 <= i <= [A|, 1 <= j <= [B|
+
+        //Theorem 3
+        //D(0, 0) = 0; D(i, 0) = summation_over_r=1_to_i(delete of A(r)); 
+        //         and D(O, j) = summation_over_r=1_to_j(insert of a character that is B(r))
+        // where 1 <= i <= [A|, 1 <= j <= [B|
+        
+        //Algorithm X
+        //D[0,0] := 0;
+        //for i := 1 to |A| do D[i, 0] := D[i - 1, 0] + cost_rm(A(i));
+        //for j := 1 to |B| do D[O, j] := D[O,j - 1] + cost_insert(B(j));
+        //for i := 1 to |A| do
+        //  for j := 1 to |B| do begin
+        //    m1 := D[i - 1, j - 1] + cost_change(A(i), B(j));
+        //    m2 := D[i - 1, j] + cost_rm(A(i));
+        //    m3 := D[i, j - 1] + cost_ins(B(j));
+        //    D[i, j] := min(m1, m2, m3);
+        //    end;
+        
+        /*
+        NOTE: this algorithm is like the LongestCommonSubsequences except that
+        for string edit: cIns=cDel=1 and cEqual=0, and the cost is minimized
+        for LCS:         cIns=cDel=0 and cEqual=1, and the cost is maximized
+        */
+        
+        //dN(i, 0) stores M(i, j − 1) and dN(i, 1) stores M(i, j)
+        for (i = 1; i <= m; ++i) {
+            //d[i][0] = i; //i*cDel
+            dN[i][0] = i; //i*cDel
+        }
+        //for (i = 1; i <= n; ++i) {
+            //d[0][i] = i; //i*cIns
+        //}
+        
+        int j;
+        int c = 0;
+        
+        int minPos;
+                
+        for (j = 1; j <= n; ++j) {
+            dN[0][1] = j; //j*cIns
+            for (i = 1; i <= m; ++i) {
+                if (a.charAt(i-1) == b.charAt(j-1)) {
+                    c = 0;
+                    System.out.printf("(i,j),  ", i-1, j-1); //subtracting 1 for 0-based indexes
+                } else {
+                    c = cChangeUnequal;
+                }
+                //d[i][j] = minimum(d[i-1][j-1] + c, d[i-1][j] + cDel, d[i][j-1] + cIns);
+                minPos = minimumPos(dN[i-1][0] + c, dN[i-1][1] + cDel, dN[i][0] + cIns);
+                switch(minPos) {
+                    case 1: {
+                        dN[i][1] = dN[i-1][1] + cDel;
+                        break;
+                    } case 2: {
+                        dN[i][1] = dN[i][0] + cIns;
+                        break;
+                    } default: {
+                        dN[i][1] = dN[i-1][0] + c; 
+                        break;
+                    }
+                }
+            }
+            System.out.printf("\n, ");
+            
+            // for i = 1 to m do Copy N[i, 0] = N[i, 1]
+            for (i = 1; i <= m; ++i) {
+                dN[i][0] = dN[i][1];
+            }
+        }
+        
+        // cannot backtrack to recover path from d, but
+        //    could presumably recover the best consistent stretches of matches.
+        //    for each j, store the (i-1, j-1) paris above where c=0.
+        //    then find the consistent stretches of matches, knowing that the
+        //    number of matches = max(string a length, string b length) - nEdits.
+        
+        return dN[m][1];
     }
     
     private int minimum(int a, int b, int c) {
@@ -139,6 +276,22 @@ public class StringEditDistance {
                 return b;
             } else {
                 return c;
+            }
+        } 
+    }
+    private int minimumPos(int a, int b, int c) {
+        if (a <= b) {
+            if (a <= c) {
+                return 0;
+            } else {
+                return 2;
+            }
+        } else {
+            // b < a
+            if (b <= c) {
+                return 1;
+            } else {
+                return 2;
             }
         } 
     }
--- tests/algorithms/strings/StringEditDistanceTest.java
+++ tests/algorithms/strings/StringEditDistanceTest.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algorithms.strings;
 
 import algorithms.util.PairIntArray;
+import java.util.Arrays;
 import junit.framework.TestCase;
 
 
@@ -15,24 +11,65 @@ import junit.framework.TestCase;
  */
 public class StringEditDistanceTest extends TestCase {
     
+    public static String reverse(String str) {
+        int n = str.length();
+        char[] r = new char[n];
+        for (int i = 0;  i < n; ++i) {
+            r[i] = str.charAt(n-i-1);
+        }
+        return new String(r);
+    }
+    
+    //String a = "rosettacode"; // 6 matches, m=11
+    //String b = "raisethysword"; // n=13
+    //int nExpected = 8;
+        
+    /*
+    //          01234567890123456
+    //             ... ....     <=== better match
+    //                  ...  ....
+    String a = "acadefcqdefabqdef";
+    //String b =       "zdefgqdef";
+    //                   ... ....
+    //             ... ....
+    String b =   "zdefgqdef"; // <=== better match for having same single gap
+    //            012345678
+    // 10,8  9,7  8,6  7,5    5,3  4,2  3,1
+    int nExpected = 10;
+    */
+    String b = "abcd";
+    String a = "zxplmn";
+    int nExpected = 6;
+    
     public StringEditDistanceTest() {
     }
     
     public void testCalculateWithWagnerFischer() {
-        String a = "rosettacode";
-        String b = "raisethysword";
         
         PairIntArray outIndexes = new PairIntArray();
         StringEditDistance sed = new StringEditDistance();
         int nEdits = sed.calculateWithWagnerFischer(a, b, outIndexes);
         
-        assertEquals(8, nEdits);
+        System.out.println("nEdits=" + nEdits);
+        assertEquals(nExpected, nEdits);
+        
+        System.out.println("outIndexes=" + outIndexes.toString());
         
         char[] result = new char[outIndexes.getN()];
         for (int i = 0; i < result.length; ++i) {
             result[i] = a.charAt(outIndexes.getX(i));
-        } 
+        }   
+        System.out.println("result=" + Arrays.toString(result));
+    }
+    
+    public void testCalculateWithModifiedWagnerFischer() {
         
+        System.out.println("testCalculateWithModifiedWagnerFischer");
+        
+        StringEditDistance sed = new StringEditDistance();
+        int nEdits = sed.calculateWithModifiedWagnerFischer(a, b);
+        System.out.println("nEdits=" + nEdits);
+        assertEquals(nExpected, nEdits);
         
     }
 }
