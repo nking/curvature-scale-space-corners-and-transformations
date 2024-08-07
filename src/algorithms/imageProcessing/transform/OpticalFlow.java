e05b283c6b38a03f7ac56cfdf2d2e413f8ded405
nking <nichole climbwithyourfeet.com>
2024-08-06 17:53:40 -0700

    editing Horn-Schunck tracking algorithm
    adding a simpler unit test for debugging the tracking algorithms

--- src/algorithms/imageProcessing/transform/OpticalFlow.java
+++ src/algorithms/imageProcessing/transform/OpticalFlow.java
@@ -1,21 +1,48 @@
 package algorithms.imageProcessing.transform;
 
+import algorithms.imageProcessing.ImageProcessor;
 import algorithms.imageProcessing.StructureTensor;
 import algorithms.imageProcessing.StructureTensorD;
 import algorithms.matrix.MatrixUtil;
+import algorithms.misc.Histogram;
+import algorithms.misc.HistogramHolder;
+import algorithms.misc.MiscMath0;
+import algorithms.statistics.Standardization;
+import algorithms.util.Errors;
+import algorithms.util.LinearRegression;
+import algorithms.util.PolygonAndPointPlotter;
+import no.uib.cipr.matrix.DenseMatrix;
+import no.uib.cipr.matrix.EVD;
 import no.uib.cipr.matrix.NotConvergedException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
+/**
+ * various optical flow algorithms.
+ *
+ * TODO: consider implementing Baker-Matthews for KLT tracker
+ */
 public class OpticalFlow {
 
     /**
+     * HS is fine for small displacements between images.
      * @param im1
      * @param im2
      * @return u and v for im1
      */
     public static List<double[][]> hornSchunck(double[][] im1, double[][] im2) {
+        return hornSchunck(im1, im2, 0, 0);
+    }
+    /**
+     * HS is fine for small displacements between images.
+     * @param im1
+     * @param im2
+     * @return u and v for im1
+     */
+    public static List<double[][]> hornSchunck(double[][] im1, double[][] im2, double uInit, double vInit) {
 
         int m = im1.length;
         int n = im1[0].length;
@@ -23,40 +50,77 @@ public class OpticalFlow {
             throw new IllegalArgumentException("im2 and im2 should have same dimensions");
         }
 
-        // image dradients:
-        StructureTensorD s1 = new StructureTensorD(im1, 1.0f, false);
-        //StructureTensorD s2 = new StructureTensorD(im1, 1.0f, false);
+        // image gradients
+        double[][] s1X = strictColumnDiff(im1);
+        double[][] s1Y = strictRowDiff(im1);
 
         // temporal differences (gradients)
-        double[][] t12 = MatrixUtil.pointwiseSubtract(im1, im2);
+        double[][] t = MatrixUtil.pointwiseSubtract(im2, im1);
 
         double[][] u = new double[m][n];
         double[][] v = new double[m][n];
+        if (uInit != 0) {
+            for (double[] uI : u) {
+                Arrays.fill(uI, uInit);
+            }
+        }
+        if (vInit != 0) {
+            for (double[] vI : v) {
+                Arrays.fill(vI, vInit);
+            }
+        }
 
-        double[][] uEst;
-        double[][] vEst;
+        double[][] uEst, uAvg;
+        double[][] vEst, vAvg;
 
-        double epsSq = 1E-8;
-        int maxIter = 200;//2_000;
+        int maxIter = 200_000;
         int nIter = 0;
 
         // smaller values of lambda for smoother flow
-        double lambda = 1./(15.*15.);//4;// 1/L = a*a
+        //double lambda = 0.5;//1./(3.*3.);//1./(15.*15.);
+        // alpha squared models an addition of moise
+        double alphaSq = 10;//Math.pow(MiscMath0.mean(MatrixUtil.rowMeans(im1)), 2);
+        System.out.println("alphaSq=" + alphaSq);
+        double epsSq = 1E-9;
 
         boolean hasConverged = false;
+
+        ImageProcessor imageProcessor = new ImageProcessor();
+
+        //double denom = (1./lambda) + dIx*dIx + dIy*dIy;
+        double[][] denom = MatrixUtil.pointwiseAdd(MatrixUtil.pointwiseMultiplication(s1X, s1X),
+            MatrixUtil.pointwiseMultiplication(s1Y, s1Y));
+        MatrixUtil.multiply(denom, alphaSq);
+
+        // average over the 4 neighbors.
+        //double[][] kernel = new double[][]{{0,0.25,0}, {0.25,0,0.25}, {0,0.25,0}};
+        // eqn 10 of "Horn-Schunck Optical Flow with a Multi-Scale Strategy"
+        //   by Meinhardt-Llopis, Sanchez & Kondermann 2012 for adding the 4 neighbors + 4 diagonal neighbors at half weight
+        double[][] kernel = new double[][]{{1./12., 1./6, 1./12.}, {1./6, 0, 1./6}, {1./12, 1./6, 1./12}};
 
         while (nIter == 0 || nIter < maxIter && !hasConverged) {
             double sumDiff = 0;
             ++nIter;
             uEst = new double[m][n];
             vEst = new double[m][n];
+
+            uAvg = MatrixUtil.copy(u);
+            vAvg = MatrixUtil.copy(v);
+            imageProcessor.applyKernel(uAvg, kernel);
+            imageProcessor.applyKernel(vAvg, kernel);
+
             for (int r = 0; r < m; ++r) {
                 for (int c = 0; c < n; ++c) {
-                    double numer = (s1.getDX()[r][c] * u[r][c]) + (s1.getDY()[r][c] * v[r][c]) + t12[r][c];
-                    double denom = (1/lambda) + s1.getDX()[r][c]*s1.getDX()[r][c] + s1.getDY()[r][c]*s1.getDY()[r][c];
-                    double nd = numer/denom;
-                    uEst[r][c] = u[r][c] - s1.getDX()[r][c] * nd;
-                    vEst[r][c] = v[r][c] - s1.getDY()[r][c] * nd;
+                    double dIx = s1X[r][c];
+                    double dIy = s1Y[r][c];
+                    double _uAvg = uAvg[r][c];
+                    double _vAvg = vAvg[r][c];
+                    double dIt = t[r][c];
+
+                    double numer = (dIx * _uAvg) + (dIy * _vAvg) + dIt;
+                    double nd = (denom[r][c] == 0.) ? 0 : numer/denom[r][c];
+                    uEst[r][c] = _uAvg - dIx * nd;
+                    vEst[r][c] = _vAvg - dIy * nd;
                     sumDiff += (Math.pow(uEst[r][c] - u[r][c], 2) + Math.pow(vEst[r][c] - v[r][c], 2));
                 }
             }
@@ -103,24 +167,41 @@ public class OpticalFlow {
      *                     patchCenters[1][0] = x1, patchCenters[1][1] = y1, ...
      *        Note, patchCenters +- pathDimension/2 should be within bounds of image.
      * @param patchDimension the width of a square patch to determine flow within. e.g. if patchDimension is 5,
-     *                       the flow is determined in 5x5 patches.
+     *                       the flow is determined in 5x5 patches.  should be >= 3.
      * @return list of uv pairs, one for each patchCenter.
      */
-    public static List<double[]> lucasKanade(double[][] im1, double[][] im2, int[][] patchCenters, int patchDimension) throws NotConvergedException {
+    public static List<double[]> lucasKanade(double[][] im1, double[][] im2, int[][] patchCenters, int patchDimension) throws NotConvergedException, IOException {
         int m = im1.length;
         int n = im1[0].length;
         if (im2.length != m || im2[0].length != n) {
             throw new IllegalArgumentException("im2 and im2 should have same dimensions");
         }
 
+        if (patchDimension < 3) {
+            throw new IllegalArgumentException("patchDimension should be >= 3");
+        }
+
+        im1 = MatrixUtil.copy(im1);
+        im2 = MatrixUtil.copy(im2);
+        double max = Math.max(MiscMath0.findMax(im1), MiscMath0.findMax(im2));
+        MatrixUtil.multiply(im1, 1./max);
+        MatrixUtil.multiply(im2, 1./max);
+
         List<double[]> uvList = new ArrayList<>();
 
+        double thresh = 0.04;
+
         // image gradients:
-        StructureTensorD s1 = new StructureTensorD(im1, 1.0f, false);
-        //StructureTensorD s2 = new StructureTensorD(im1, 1.0f, false);
+        // image gradients
+        double[][] s1X = strictColumnDiff(im1);
+        double[][] s1Y = strictRowDiff(im1);
+
+        double[] u = new double[patchCenters.length];
+        double[] v = new double[patchCenters.length];
+        int uvI = 0;
 
         // temporal differences (gradients)
-        double[][] t12 = MatrixUtil.pointwiseSubtract(im1, im2);
+        double[][] t = MatrixUtil.pointwiseSubtract(im2, im1);
         for (int[] pc : patchCenters) {
             // build A [patchDimension*patchDimension x 2]
             double[][] A = new double[2][2];
@@ -129,25 +210,76 @@ public class OpticalFlow {
             for (int dX = -patchDimension/2; dX <= patchDimension/2; ++dX) {
                 if ((patchDimension & 1) != 1 && dX == patchDimension/2) continue;
                 int c = pc[0] + dX;
-                if (c < 0 || c >= im2[0].length) throw new IllegalArgumentException("out of bounds of width: " + c);
+                if (c < 0 || c >= im2[0].length)
+                    throw new IllegalArgumentException("col " + c + " out of bounds of width: " + c);
                 for (int dY = -patchDimension/2; dY <= patchDimension/2; ++dY) {
                     if ((patchDimension & 1) != 1 && dY == patchDimension/2) continue;
                     int r = pc[1] + dY;
-                    if (r < 0 || r >= im2.length) throw new IllegalArgumentException("out of bounds of height: " + r);
+                    if (r < 0 || r >= im2.length)
+                        throw new IllegalArgumentException("row " + r + " out of bounds of height: " + r);
 
-                    A[0][0] += (s1.getDX()[r][c] * s1.getDX()[r][c]);
-                    A[0][1] += (s1.getDX()[r][c] * s1.getDY()[r][c]);
-                    A[1][1] += (s1.getDY()[r][c] * s1.getDY()[r][c]);
-                    A[1][0] = A[0][1];
-
-                    b[0] += s1.getDX()[r][c] * t12[r][c];
-                    b[1] += s1.getDY()[r][c] * t12[r][c];
+                    A[0][0] += (s1X[r][c] * s1X[r][c]);
+                    A[0][1] += (s1X[r][c] * s1Y[r][c]);
+                    A[1][1] += (s1Y[r][c] * s1Y[r][c]);
+                    b[0] += s1X[r][c] * t[r][c];
+                    b[1] += s1Y[r][c] * t[r][c];
                 } // end for loop dY
             }// end for loop dX
+            A[1][0] = A[0][1];
             //xEst = pseudoInv(A) * b // [2x2] * [2x1] = [2X1]
+
+            //A^T*A should be invertible
+            //A^T*A should not be too small (length >= 2)
+            // eigenvalue1/eigenvalue2 should not be too large
+
+            // can use Harris corners response > 0 to ensure good patch to include
+            double detA = MatrixUtil.determinant(A);
+            double trA = MatrixUtil.trace(A);
+            boolean cont = (detA - thresh * trA * trA) > 0;
+
+            /* or that there is power in eigen[1], hence rank =2
+            double[][] ATA = MatrixUtil.createATransposedTimesA(A);
+            EVD evd = no.uib.cipr.matrix.EVD.factorize(new DenseMatrix(ATA));
+            double[] eigenValues = evd.getRealEigenvalues();
+            boolean cont = eigenValues[eigenValues.length - 1] >= thresh;*/
+            if (!cont)
+                continue;
+
             double[] uv = MatrixUtil.multiplyMatrixByColumnVector(MatrixUtil.pseudoinverseFullRowRank(A), b);
             uvList.add(uv);
+
+            u[uvI] = uv[0];
+            v[uvI] = uv[1];
+            ++uvI;
         }
+
+        //Returns: 2 dimensional * array with row 0 being the bin centers, and row 1 being the histogram counts
+        double[][] uH = Histogram.createHistogram(u, 10);
+        double[][] vH = Histogram.createHistogram(v, 10);
+        PolygonAndPointPlotter plotter = new PolygonAndPointPlotter();
+        plotter.addPlot(uH[0], uH[1],null,null, "U");
+        plotter.addPlot(vH[0], vH[1],null,null, "V");
+        plotter.writeFile();
+
         return uvList;
+    }
+
+    protected static double[][] strictColumnDiff(double[][] a) {
+        double[][] b = new double[a.length][a[0].length];
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; j < b.length-1; ++j) {
+                b[i][j] = a[i][j+1] - a[i][j];
+            }
+        }
+        return b;
+    }
+    protected static double[][] strictRowDiff(double[][] a) {
+        double[][] b = new double[a.length][a[0].length];
+        for (int i =0; i < a.length-1; ++i) {
+            for (int j = 0; j < b.length; ++j) {
+                b[i][j] = a[i+1][j] - a[i][j];
+            }
+        }
+        return b;
     }
 }
--- tests/algorithms/imageProcessing/transform/OpticalFlowTest.java
+++ tests/algorithms/imageProcessing/transform/OpticalFlowTest.java
@@ -5,9 +5,11 @@ import algorithms.imageProcessing.features.PhaseCongruencyDetector;
 import algorithms.imageProcessing.features.orb.ORB;
 import algorithms.imageProcessing.segmentation.NormalizedCuts;
 import algorithms.imageProcessing.segmentation.SLICSuperPixels;
+import algorithms.matrix.MatrixUtil;
 import algorithms.misc.MiscDebug;
 import algorithms.misc.MiscMath;
 import algorithms.misc.MiscMath0;
+import algorithms.util.FormatArray;
 import algorithms.util.PairInt;
 import algorithms.util.ResourceFinder;
 import junit.framework.TestCase;
@@ -24,12 +26,19 @@ public class OpticalFlowTest extends TestCase {
     public OpticalFlowTest() {
     }
     
-    public void testHornSchunck() throws Exception {
+    public void estHornSchunck() throws Exception {
 
         // each 300x225
-        String[] fileNames = new String[]{"tomatoes_01.png", /*"tomatoes_02.png",*/ "tomatoes_03.png"};
+        String[] fileNames = new String[]{"tomatoes_01.png", "tomatoes_02.png", /*"tomatoes_03.png"*/};
+        /*
+        a feature in the 3 tomatoe images:
+        177, 140  in 01.png
+        177, 139  in 02.png
+        157, 135  in 03.png
+         */
 
-        List<double[][]> images = new ArrayList<>();
+        double uInit = 0;
+        double vInit = 0;
 
         for (int i = 0; i < fileNames.length-1; ++i) {
             String fileName = fileNames[i];
@@ -48,20 +57,26 @@ public class OpticalFlowTest extends TestCase {
             //double[][] b2 = img.exportHSBRowMajor().get(2);
             double[][] b2 = img.exportRowMajor().get(0);
 
-            List<double[][]> uvHS = OpticalFlow.hornSchunck(b1, b2);
+            List<double[][]> uvHS = OpticalFlow.hornSchunck(b1, b2, uInit, vInit);
 
             int t = 2;
         }
     }
 
-    public void testLucasKanade() throws Exception {
+    public void estLucasKanade() throws Exception {
 
         // each 300x225
         String[] fileNames = new String[]{"tomatoes_01.png", /*"tomatoes_02.png",*/ "tomatoes_03.png"};
-
+        /*
+        a feature in the 3 tomatoe images:
+        177, 140  in 01.png
+        177, 139  in 02.png
+        157, 135  in 03.png
+         */
         List<double[][]> images = new ArrayList<>();
 
-        int nCorners = 500;
+        int nCorners = 300;
+        int patchSize = 30;
 
         ImageProcessor imageProcessor = new ImageProcessor();
         CannyEdgeFilterAdaptive canny;
@@ -91,7 +106,7 @@ public class OpticalFlowTest extends TestCase {
 
 
             // x, y
-            List<PairInt> kp1 = findKeyPoints(img, nCorners);
+            List<PairInt> kp1 = findKeyPoints(img, nCorners, patchSize);
 
             int[][] patchCenters = new int[kp1.size()][2];
             for (int ii = 0; ii < kp1.size(); ++ii) {
@@ -104,27 +119,45 @@ public class OpticalFlowTest extends TestCase {
             img = ImageIOHelper.readImageExt(filePath);
 
             double[][] b2 = img.exportHSBRowMajor().get(2);
-            //GreyscaleImage = luvTheta = imageProcessor.createCIELUVTheta(img, 255);
-            //double[][] b2 = luvTheta.exportRowMajor();
-            /*
-            canny = new CannyEdgeFilterAdaptive();
-            canny.overrideToUseAdaptiveThreshold();
-            canny.overrideToNotUseLineThinner();
-            canny.applyFilter(img.copyToGreyscale2());
-            prod = canny.getFilterProducts();
-            gsCanny = prod.getGradientXY();
-            gsCanny.normalizeToMax255();
-            double[][] b2 = gsCanny.exportRowMajor();
-            */
 
-            List<double[]> uvLK = OpticalFlow.lucasKanade(b1, b2, patchCenters, 5);
+            // expect u=-20, v=-5 for 01 to 03.   large motion, so needs a multiplscal pyrimdal approach
+            List<double[]> uvLK = OpticalFlow.lucasKanade(b1, b2, patchCenters, patchSize);
 
             int t = 1;
 
         }
     }
 
-    private List<PairInt> findKeyPoints(ImageExt img, int nCorners) {
+    public void test2() {
+        int m = 10, n = m;
+        double[][] im1 = new double[m][n];
+        double[][] im2 = new double[m][n];
+        double deltaI = 1./m;
+        for (int i = 1; i < m; ++i) {
+            double b = deltaI*i;
+            for (int j = 0; j <= i; ++j) {
+                im1[i][j] = b;
+                im1[j][i] = b;
+
+                if (i+1 >= 0 && i+1 < m && j+1 >= 0 && j+1 < n) {
+                    im2[i + 1][j + 1] = b;
+                }
+                if (j+1 >= 0 && j+1 < m && i+1 >= 0 && i+1 < n) {
+                    im2[j + 1][i + 1] = b;
+                }
+            }
+        }
+        double uInit = 0;//0.5;
+        double vInit = 0;//0.5;
+
+        List<double[][]> uvHS = OpticalFlow.hornSchunck(im1, im2, uInit, vInit);
+
+        System.out.printf("u=%s\n", FormatArray.toString(uvHS.get(0), "%.3f"));
+        System.out.printf("v=%s\n", FormatArray.toString(uvHS.get(1), "%.3f"));
+
+    }
+
+    private List<PairInt> findKeyPoints(ImageExt img, int nCorners, int patchSize) {
         //double[][] b1 = luvTheta.exportRowMajor();
 
             /*canny = new CannyEdgeFilterAdaptive();
@@ -142,11 +175,16 @@ public class OpticalFlowTest extends TestCase {
         PhaseCongruencyDetector.PhaseCongruencyProducts products =
                 phaseCDetector.phaseCongMono(img.copyToGreyscale2());
 
+        int buffer = 5;
+        int len = buffer + patchSize/2;
+
         int[][] thinned = products.getThinned();
         GreyscaleImage edges = img.createWithDimensions().copyToGreyscale();
         for (int i = 0; i < img.getWidth(); ++i) {
             for (int j = 0; j < img.getHeight(); ++j) {
                 if (thinned[j][i] > 0) {
+                    if (i - len < 0 || i+len >= img.getWidth() || j - len < 0 || j+len >= img.getHeight())
+                        continue;
                     edges.setValue(i, j, thinned[j][i]);
                 }
             }
@@ -162,7 +200,11 @@ public class OpticalFlowTest extends TestCase {
 
         MiscDebug.writeImage(edges, "_thinned_" + "_" + "_phasecong_" + "_");
         for (int i = 0; i < kp1.size(); ++i) {
-            ImageIOHelper.addPointToImage(kp1.get(i).getX(), kp1.get(i).getY(), tmp, 2, 255, 0, 0);
+            int x = kp1.get(i).getX();
+            int y = kp1.get(i).getY();
+            if (x - len < 0 || x+len >= img.getWidth() || y - len < 0 || y+len >= img.getHeight())
+                continue;
+            ImageIOHelper.addPointToImage(x, y, tmp, 2, 255, 0, 0);
         }
         MiscDebug.writeImage(tmp, "_thinned_" + "_" + "_phasecong_kp_" + "_");
 
