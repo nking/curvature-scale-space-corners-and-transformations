ccf2e4e87c635483cc8acb94dc906595fd1790c5
nking <nichole.king@gmail.com>
2025-08-01 10:56:20 -0700

    adding validation and tests to the FasterRCNN notebook.  writing output images and detected objects to files.

--- src/load_pytorch_models.py
+++ src/load_pytorch_models.py
@@ -52,3 +52,27 @@ def get_transform(train:bool=True):
     transforms.append(T.ToDtype(torch.float, scale=True))
     transforms.append(T.ToPureTensor())
     return T.Compose(transforms)
+
+class EarlyStopping:
+    '''
+    from https://medium.com/biased-algorithms/a-practical-guide-to-implementing-early-stopping-in-pytorch-for-model-training-99a7cbd46e9d
+    '''
+    def __init__(self, patience=5, delta=0, verbose=False):
+        self.patience = patience
+        self.delta = delta
+        self.verbose = verbose
+        self.best_loss = None
+        self.no_improvement_count = 0
+        self.stop_training = False
+    
+    def check_early_stop(self, val_loss):
+        if self.best_loss is None or val_loss < self.best_loss - self.delta:
+            self.best_loss = val_loss
+            self.no_improvement_count = 0
+        else:
+            self.no_improvement_count += 1
+            if self.no_improvement_count >= self.patience:
+                self.stop_training = True
+                if self.verbose:
+                    print("Stopping early as no improvement has been observed.")
+                    
